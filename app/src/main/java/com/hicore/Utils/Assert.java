f389f77d372727d6bc1db3e79c180f7964485c5e
Hicores <hicores@163.com>
2022-03-06 19:53:52 +0800

    测试提交

--- app/src/main/java/com/hicore/Utils/Assert.java
+++ app/src/main/java/com/hicore/Utils/Assert.java
@@ -3,6 +3,5 @@ package com.hicore.Utils;
 public class Assert {
     public static void notNull(Object obj,String message){
         if (obj == null)throw new NullPointerException(message);
-
     }
 }
