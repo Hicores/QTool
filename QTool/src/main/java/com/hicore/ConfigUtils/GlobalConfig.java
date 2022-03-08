6aed9bd99b6d52966bb076376e15d5c9d24d656d
Hicores <hicores@163.com>
2022-03-08 16:16:54 +0800

    添加 GlobalConfig 工具类

--- QTool/src/main/java/com/hicore/ConfigUtils/GlobalConfig.java
+++ QTool/src/main/java/com/hicore/ConfigUtils/GlobalConfig.java
@@ -1,4 +1,41 @@
 package com.hicore.ConfigUtils;
 
+import android.content.SharedPreferences;
+
+import com.hicore.qtool.XposedInit.HookEnv;
+
 public class GlobalConfig {
+    private static String TAG = "QTool";
+    public static void Put_String(String Name,String Value){
+        SharedPreferences share = HookEnv.AppContext.getSharedPreferences(TAG,0);
+        SharedPreferences.Editor editor = share.edit();
+        editor.putString(Name,Value);
+        editor.commit();
+    }
+    public static String Get_String(String Name){
+        SharedPreferences share = HookEnv.AppContext.getSharedPreferences(TAG,0);
+        return share.getString(Name,"");
+    }
+
+    public static void Put_Boolean(String Name,boolean Value){
+        SharedPreferences share = HookEnv.AppContext.getSharedPreferences(TAG,0);
+        SharedPreferences.Editor editor = share.edit();
+        editor.putBoolean(Name,Value);
+        editor.commit();
+    }
+    public static boolean Get_Boolean(String Name,boolean Defvalue){
+        SharedPreferences share = HookEnv.AppContext.getSharedPreferences(TAG,0);
+        return share.getBoolean(Name,Defvalue);
+    }
+
+    public static void Put_Long(String Name,long Value){
+        SharedPreferences share = HookEnv.AppContext.getSharedPreferences(TAG,0);
+        SharedPreferences.Editor editor = share.edit();
+        editor.putLong(Name,Value);
+        editor.commit();
+    }
+    public static long Get_Long(String Name,long Defvalue){
+        SharedPreferences share = HookEnv.AppContext.getSharedPreferences(TAG,0);
+        return share.getLong(Name,Defvalue);
+    }
 }
