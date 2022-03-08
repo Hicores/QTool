5bb68ae7ae6096a5e46a2c8fd318ce0f5a6aadfd
Hicores <hicores@163.com>
2022-03-08 15:45:50 +0800

    添加工具类 XPBridge

---
+++ QTool/src/main/java/com/hicore/HookUtils/XPBridge.java
@@ -0,0 +1,56 @@
+package com.hicore.HookUtils;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.atomic.AtomicReference;
+
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+
+public class XPBridge {
+    public static void HookBefore(Method m,BeforeHook before){
+        XposedBridge.hookMethod(m, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                super.beforeHookedMethod(param);
+                before.onBefore(param);
+            }
+        });
+    }
+    public static void HookAfter(Method m,AfterHook after){
+        XposedBridge.hookMethod(m, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                super.beforeHookedMethod(param);
+                after.onAfter(param);
+            }
+        });
+    }
+    public static void HookBeforeOnce(Method m,BeforeHook before){
+        AtomicReference<XC_MethodHook.Unhook> cacheUnHook = new AtomicReference<>();
+        cacheUnHook.set(XposedBridge.hookMethod(m, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                super.beforeHookedMethod(param);
+                before.onBefore(param);
+                cacheUnHook.get().unhook();
+            }
+        }));
+    }
+    public static void HookAfterOnce(Method m,AfterHook after){
+        AtomicReference<XC_MethodHook.Unhook> cacheUnHook = new AtomicReference<>();
+        cacheUnHook.set(XposedBridge.hookMethod(m, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                super.beforeHookedMethod(param);
+                after.onAfter(param);
+                cacheUnHook.get().unhook();
+            }
+        }));
+    }
+    public interface BeforeHook{
+        void onBefore(XC_MethodHook.MethodHookParam param);
+    }
+    public interface AfterHook{
+        void onAfter(XC_MethodHook.MethodHookParam param);
+    }
+}
--- QTool/src/main/java/com/hicore/qtool/XPWork/DebugSetInject/DebugSetHook.java
+++ QTool/src/main/java/com/hicore/qtool/XPWork/DebugSetInject/DebugSetHook.java
@@ -1,9 +1,19 @@
 package com.hicore.qtool.XPWork.DebugSetInject;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
 import com.hicore.HookItem;
+import com.hicore.HookUtils.XPBridge;
+import com.hicore.ReflectUtils.MClass;
+import com.hicore.ReflectUtils.MMethod;
 import com.hicore.qtool.XposedInit.ItemLoader.BaseHookItem;
 
 import java.lang.reflect.Method;
+
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
 
 @HookItem(isDelayInit = false,isRunInAllProc = false)
 public class DebugSetHook extends BaseHookItem {
@@ -14,6 +24,8 @@ public class DebugSetHook extends BaseHookItem {
 
     @Override
     public boolean startHook() {
+        Method hookMethod = getHookMethod();
+
         return super.startHook();
     }
 
@@ -37,6 +49,10 @@ public class DebugSetHook extends BaseHookItem {
         return super.isLoaded();
     }
     public Method getHookMethod(){
-        return null;
+        Method m = MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.settings.message.AssistantSettingFragment"),
+                "doOnCreateView",void.class,new Class[]{
+                        LayoutInflater.class, ViewGroup.class, Bundle.class
+                });
+        return m;
     }
 }
