7463d3ec3a126162f9160a86f1d0ea4176404edd
Hicores <hicores@163.com>
2022-03-08 15:00:28 +0800

    添加Auto Test Build

---
+++ QTool/src/main/java/com/hicore/qtool/XPWork/DebugSetInject/DebugSetHook.java
@@ -0,0 +1,46 @@
+package com.hicore.qtool.XPWork.DebugSetInject;
+
+import android.content.IntentFilter;
+
+import com.hicore.HookItem;
+import com.hicore.ReflectUtils.MClass;
+import com.hicore.ReflectUtils.MMethod;
+import com.hicore.qtool.XposedInit.ItemLoader.BaseHookItem;
+
+import java.lang.reflect.Method;
+
+@HookItem(isDelayInit = false,isRunInAllProc = false)
+public class DebugSetHook extends BaseHookItem {
+    @Override
+    public String getTag() {
+        return super.getTag();
+    }
+
+    @Override
+    public boolean startHook() {
+        return super.startHook();
+    }
+
+    @Override
+    public boolean isEnable() {
+        return super.isEnable();
+    }
+
+    @Override
+    public String getErrorInfo() {
+        return "No Info";
+    }
+
+    @Override
+    public boolean check() {
+        return super.check();
+    }
+
+    @Override
+    public boolean isLoaded() {
+        return super.isLoaded();
+    }
+    public Method getHookMethod(){
+        Method m = MMethod.FindMethod(MClass.loadClass(""))
+    }
+}
