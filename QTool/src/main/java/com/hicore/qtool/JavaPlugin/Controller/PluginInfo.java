fc90fe5d856aa0ac11287ef8462605ac60e35c79
Hicores <hicores@163.com>
2022-03-10 13:54:54 +0800

    添加 PluginInfo

--- QTool/src/main/java/com/hicore/qtool/JavaPlugin/Controller/PluginInfo.java
+++ QTool/src/main/java/com/hicore/qtool/JavaPlugin/Controller/PluginInfo.java
@@ -1,11 +1,38 @@
 package com.hicore.qtool.JavaPlugin.Controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import bsh.Interpreter;
 
 public class PluginInfo {
     public String PluginID;
+    public String PluginVerifyID;
     public String LocalPath;
     public boolean IsBlackMode;
     public ArrayList<String> ListStr;
+
+    public boolean IsLoadSuccess;
+    public boolean IsLoading;
+
+    public String PluginName;
+    public String PluginAuthor;
+    public String PluginVersion;
+    public Interpreter Instance;
+    public HashMap<String,FunctionInfo> ItemFunctions = new HashMap<>();
+
+    public boolean IsAvailable(String GroupUin,int isTroop){
+        if (isTroop == 1 || isTroop == 10014){
+            return IsBlackMode != ListStr.contains(GroupUin);
+        }
+        return true;
+    }
+
+    public static class FunctionInfo{
+        public String FunctionName;
+        public String FunctionCallback;
+
+        public int FunctionType;
+    }
 
 }
---
+++ QTool/src/main/java/com/hicore/qtool/JavaPlugin/Controller/PluginMethod.java
@@ -0,0 +1,8 @@
+package com.hicore.qtool.JavaPlugin.Controller;
+
+public class PluginMethod {
+    private PluginInfo info_;
+    public PluginMethod(PluginInfo cacheInfo){
+        info_ = cacheInfo;
+    }
+}
