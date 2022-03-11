65b92b02bf8d44c0b69594a8aeb95999b0328872
Hicores <hicores@163.com>
2022-03-11 14:28:18 +0800

    完成 QQGroupManager

---
+++ QTool/src/main/java/com/hicore/qtool/QQManager/QQGuildManager.java
@@ -0,0 +1,11 @@
+package com.hicore.qtool.QQManager;
+
+import android.text.TextUtils;
+
+public class QQGuildManager {
+    public static void Guild_Mute(String GuildID,String UserTinyID,long Time){
+        if (TextUtils.isEmpty(UserTinyID)){
+
+        }
+    }
+}
