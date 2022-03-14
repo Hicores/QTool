2f840f8f9bcd49aa527909844748ddb61d696d97
Hicores <hicores@163.com>
2022-03-14 17:09:30 +0800

    为 脚本存储配置接口 添加实现

--- QTool/src/main/java/com/hicore/qtool/JavaPlugin/Controller/PluginStoreUtils.java
+++ QTool/src/main/java/com/hicore/qtool/JavaPlugin/Controller/PluginStoreUtils.java
@@ -1,29 +1,106 @@
 package com.hicore.qtool.JavaPlugin.Controller;
 
+import com.hicore.Utils.DataUtils;
+import com.hicore.Utils.FileUtils;
+import com.hicore.qtool.HookEnv;
+
+import org.json.JSONObject;
+
+import java.io.File;
+
 public class PluginStoreUtils {
+    private static void reqPath(){
+        String Path = HookEnv.ExtraDataPath + "/PluginConfig/";
+        if (!new File(Path).exists()){
+            new File(Path).mkdirs();
+
+        }
+    }
     public static String getString(String PluginID,String key){
+        reqPath();
+        String Path = HookEnv.ExtraDataPath + "/PluginConfig/"+ PluginID.hashCode()+".json";
+        try{
+            JSONObject itemJSON = new JSONObject(FileUtils.ReadFileString(Path));
+            return itemJSON.getString(key);
+        }catch (Exception e){
+        }
         return null;
     }
     public static void putString(String PluginID,String key,String value){
+        try{
+            reqPath();
+            String Path = HookEnv.ExtraDataPath + "/PluginConfig/"+ PluginID.hashCode()+".json";
+            JSONObject itemJSON = new JSONObject(FileUtils.ReadFileString(Path));
+            itemJSON.put(key,value);
+            FileUtils.WriteToFile(Path,itemJSON.toString());
+        }catch (Exception e){
+
+        }
+
 
     }
     public static boolean getBoolean(String PluginID,String key,boolean defValue){
-        return false;
+        String Path = HookEnv.ExtraDataPath + "/PluginConfig/"+ PluginID.hashCode()+".json";
+        try{
+            reqPath();
+            JSONObject itemJSON = new JSONObject(FileUtils.ReadFileString(Path));
+            return itemJSON.getBoolean(key);
+        }catch (Exception e){
+        }
+        return defValue;
     }
     public static void putBoolean(String PluginID,String key,boolean value){
+        try{
+            reqPath();
+            String Path = HookEnv.ExtraDataPath + "/PluginConfig/"+ PluginID.hashCode()+".json";
+            JSONObject itemJSON = new JSONObject(FileUtils.ReadFileString(Path));
+            itemJSON.put(key,value);
+            FileUtils.WriteToFile(Path,itemJSON.toString());
+        }catch (Exception e){
 
+        }
     }
     public static int getInt(String PluginID,String key,int defValue){
-        return 0;
+        String Path = HookEnv.ExtraDataPath + "/PluginConfig/"+ PluginID.hashCode()+".json";
+        try{
+            reqPath();
+            JSONObject itemJSON = new JSONObject(FileUtils.ReadFileString(Path));
+            return itemJSON.getInt(key);
+        }catch (Exception e){
+        }
+        return defValue;
     }
     public static void putInt(String PluginID,String key,int value){
+        try{
+            reqPath();
+            String Path = HookEnv.ExtraDataPath + "/PluginConfig/"+ PluginID.hashCode()+".json";
+            JSONObject itemJSON = new JSONObject(FileUtils.ReadFileString(Path));
+            itemJSON.put(key,value);
+            FileUtils.WriteToFile(Path,itemJSON.toString());
+        }catch (Exception e){
 
+        }
     }
     public static long getLong(String PluginID,String key,long value)
     {
-        return 0;
+        String Path = HookEnv.ExtraDataPath + "/PluginConfig/"+ PluginID.hashCode()+".json";
+        try{
+            reqPath();
+            JSONObject itemJSON = new JSONObject(FileUtils.ReadFileString(Path));
+            return itemJSON.getLong(key);
+        }catch (Exception e){
+        }
+        return value;
     }
     public static void putLong(String PluginID,String key,long value){
+        try{
+            reqPath();
+            String Path = HookEnv.ExtraDataPath + "/PluginConfig/"+ PluginID.hashCode()+".json";
+            JSONObject itemJSON = new JSONObject(FileUtils.ReadFileString(Path));
+            itemJSON.put(key,value);
+            FileUtils.WriteToFile(Path,itemJSON.toString());
+        }catch (Exception e){
 
+        }
     }
 }
