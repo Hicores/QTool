f28bd8cac402cb76c5eedd116dee54e6ab9df2c4
Hicores <hicores@163.com>
2022-04-11 20:04:09 +0800

    判断isFirstApplication

--- QTool/src/main/java/com/hicore/qtool/XposedInit/HookEntry.java
+++ QTool/src/main/java/com/hicore/qtool/XposedInit/HookEntry.java
@@ -1,20 +1,13 @@
 package com.hicore.qtool.XposedInit;
 
-import android.app.Fragment;
-import android.content.Context;
-import android.os.Bundle;
 
 import com.github.kyuubiran.ezxhelper.init.EzXHelperInit;
-import com.hicore.LogUtils.LogUtils;
 import com.hicore.qtool.HookEnv;
-import com.hicore.qtool.QQTools.ContUtil;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
-import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
@@ -22,15 +15,18 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class HookEntry implements IXposedHookLoadPackage, IXposedHookZygoteInit {
     private static StartupParam cacheParam;
     @Override
-    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        if (cacheParam == null){
-            XposedBridge.log("[QTool]initZygote may not be invoke, please check your Xposed Framework!");
-            return;
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam){
+        if (lpparam.isFirstApplication){
+            if (cacheParam == null){
+                XposedBridge.log("[QTool]initZygote may not be invoke, please check your Xposed Framework!");
+                return;
+            }
+            InjectClassLoader();
+            XposedBridge.log("[QTool]Load from "+lpparam.processName);
+            FixSubLoadClass.loadZygote(cacheParam);
+            FixSubLoadClass.loadPackage(lpparam);
         }
-        InjectClassLoader();
-        XposedBridge.log("[QTool]Load from "+lpparam.processName);
-        FixSubLoadClass.loadZygote(cacheParam);
-        FixSubLoadClass.loadPackage(lpparam);
+
 
     }
     @Override
