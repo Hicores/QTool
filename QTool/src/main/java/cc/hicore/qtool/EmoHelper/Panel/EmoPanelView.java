f0c1787ce3cfe0731ee970be05bd987815b8fb10
Hicores <hicores@163.com>
2022-04-24 13:22:23 +0800

    更新 表情面板

--- QTool/src/main/java/cc/hicore/qtool/EmoHelper/Panel/EmoPanelView.java
+++ QTool/src/main/java/cc/hicore/qtool/EmoHelper/Panel/EmoPanelView.java
@@ -3,6 +3,7 @@ package cc.hicore.qtool.EmoHelper.Panel;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Handler;
 import android.os.Looper;
@@ -28,6 +29,7 @@ import cc.hicore.Utils.Utils;
 import cc.hicore.qtool.EmoHelper.CloudSync.SyncCore;
 import cc.hicore.qtool.EmoHelper.Hooker.RepeatWithPic;
 import cc.hicore.qtool.HookEnv;
+import cc.hicore.qtool.QQManager.QQEnvUtils;
 import cc.hicore.qtool.QQMessage.QQMsgBuilder;
 import cc.hicore.qtool.QQMessage.QQMsgSender;
 
@@ -174,12 +176,21 @@ public class EmoPanelView extends BottomPopupView {
 
 
         TextView view = new TextView(getContext());
-        view.setText("+");
+        view.setText("查看分享");
         view.setTextColor(Color.parseColor("#99FFFF"));
         view.setTextSize(24);
         LinearLayout.LayoutParams parans = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         parans.setMargins(Utils.dip2px(getContext(),10),0,Utils.dip2px(getContext(),10),0);
         view.setOnClickListener(v-> searchOnline());
+        PathBar.addView(view,parans);
+
+        view = new TextView(getContext());
+        view.setText("从TG下载");
+        view.setTextColor(Color.parseColor("#99FFFF"));
+        view.setTextSize(24);
+        parans = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        parans.setMargins(Utils.dip2px(getContext(),10),0,Utils.dip2px(getContext(),10),0);
+        view.setOnClickListener(v-> searchFromTg());
         PathBar.addView(view,parans);
 
         int width = View.MeasureSpec.makeMeasureSpec(0,
@@ -366,15 +377,17 @@ public class EmoPanelView extends BottomPopupView {
                                 e.printStackTrace();
                             }
                         });
-                    } catch (JSONException e) {
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
                 }).start();
+
+
             }
 
             @Override
             protected int getPopupWidth() {
-                return XPopupUtils.getScreenHeight(getContext());
+                return XPopupUtils.getScreenWidth(getContext());
             }
             @Override
             protected int getMaxHeight() {
@@ -391,6 +404,182 @@ public class EmoPanelView extends BottomPopupView {
                 .popupAnimation(PopupAnimation.TranslateFromTop)
                 .asCustom(view)
                 .show();
+    }
+    private void searchFromTg(){
+        PositionPopupView view = new PositionPopupView(getContext()){
+            @Override
+            protected int getImplLayoutId() {
+                return R.layout.emo_get_from_tg;
+            }
+
+            @Override
+            protected void onCreate() {
+                super.onCreate();
+                LinearLayout mRoot = findViewById(R.id.Bundle_Content_List);
+                TextView text = new TextView(getContext());
+                text.setText("正在加载...");
+                mRoot.addView(text);
+
+                new Thread(()->{
+                    String Content = HttpUtils.getContent("https://tg.haonb.cc/getByUin?uin="+ QQEnvUtils.getCurrentUin());
+                    try {
+                        JSONObject json = new JSONObject(Content);
+                        new Handler(Looper.getMainLooper()).post(()->{
+                            mRoot.removeAllViews();
+                            try {
+                                JSONArray itemArr = json.getJSONArray("data");
+                                LinearLayout mChild = null;
+                                for(int i=0;i<itemArr.length();i++){
+                                    if (i % 3 == 0){
+                                        mChild = new LinearLayout(getContext());
+                                        mRoot.addView(mChild);
+                                    }
+                                    JSONObject bundleInfo = itemArr.getJSONObject(i);
+                                    String coverPath = "https://cdn.haonb.cc/"+bundleInfo.optString("cover");
+                                    String ID = bundleInfo.getString("ID");
+                                    LinearLayout.LayoutParams paramab = new LinearLayout.LayoutParams(XPopupUtils.getScreenWidth(getContext())/3, ViewGroup.LayoutParams.WRAP_CONTENT);
+                                    paramab.setMargins(0,10,0,10);
+
+                                    LinearLayout mItem = new LinearLayout(getContext());
+                                    mItem.setGravity(Gravity.LEFT);
+                                    mItem.setOrientation(LinearLayout.VERTICAL);
+                                    mChild.addView(mItem,paramab);
+
+                                    ImageView cover = new ImageView(getContext());
+                                    Glide.with(HookEnv.AppContext)
+                                            .load(new URL(coverPath))
+                                            .fitCenter()
+                                            .into(cover);
+                                    cacheImageView.add(cover);
+
+                                    LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(XPopupUtils.getScreenWidth(getContext())/4,XPopupUtils.getScreenWidth(getContext())/4);
+                                    param.setMargins(0,10,0,10);
+                                    mItem.addView(cover,param);
+
+                                    cover.setOnClickListener(v-> {
+                                        LoadFromTgOnlinePicList(ID);
+                                        dismiss();
+                                    });
+
+                                    TextView title = new TextView(getContext());
+                                    title.setText(bundleInfo.getString("name"));
+                                    mItem.addView(title);
+                                    TextView size = new TextView(getContext());
+                                    size.setText("总大小:"+Utils.bytes2kb(bundleInfo.optLong("size")));
+                                    mItem.addView(size);
+
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        });
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }).start();
+
+                Button btnGetFromID = findViewById(R.id.getFromID);
+                btnGetFromID.setOnClickListener(v->{
+                    EditText ed = new EditText(getContext());
+                    new AlertDialog.Builder(getContext(),Utils.getDarkModeStatus(getContext()) ? AlertDialog.THEME_HOLO_DARK : AlertDialog.THEME_HOLO_LIGHT)
+                            .setTitle("输入ID")
+                            .setView(ed)
+                            .setNeutralButton("确定", (dialog, which) -> {
+                                String result = HttpUtils.getContent("https://tg.haonb.cc/getByID?id="+ed.getText().toString());
+                                try {
+                                    JSONObject mre= new JSONObject(result);
+                                    if (mre.getInt("code") == 1){
+                                        Utils.ShowToast(mre.getString("msg"));
+                                        return;
+                                    }
+                                    LoadFromTgOnlinePicList(mre.getJSONObject("data").getString("ID"));
+                                    dismiss();
+                                } catch (JSONException e) {
+                                    Utils.ShowToast("发生错误:\n"+e);
+                                }
+                            }).show();
+
+                });
+            }
+
+            @Override
+            protected int getPopupWidth() {
+                return XPopupUtils.getScreenWidth(getContext());
+            }
+            @Override
+            protected int getMaxHeight() {
+                return (int) (XPopupUtils.getScreenHeight(getContext()) * .8f);
+            }
+            @Override
+            protected int getPopupHeight() {
+                return (int) (XPopupUtils.getScreenHeight(getContext()) * .8f);
+            }
+
+        };
+        new XPopup.Builder(getContext())
+                .isDestroyOnDismiss(true)
+                .popupAnimation(PopupAnimation.TranslateFromTop)
+                .asCustom(view)
+                .show();
+    }
+    private void LoadFromTgOnlinePicList(String ID){
+        Context context = getContext();
+        ProgressDialog dialog = new ProgressDialog(context,3);
+        dialog.setTitle("正在加载...");
+        dialog.setMessage("正在加载图片列表...");
+        dialog.setCancelable(false);
+        dialog.show();
+        new Thread(()->{
+            try{
+                String listData = HttpUtils.getContent("https://tg.haonb.cc/getContent?id="+ID);
+                JSONObject newJson = new JSONObject(listData);
+                JSONArray dataArr = newJson.getJSONArray("data");
+                new Handler(Looper.getMainLooper()).post(()->{
+                    try{
+                        data.clear();
+                        btnSaveToLocal.setVisibility(VISIBLE);
+                        for (int i=0;i<dataArr.length();i++){
+                            JSONObject item = dataArr.getJSONObject(i);
+                            EmoPanel.EmoInfo infoItem = new EmoPanel.EmoInfo();
+                            infoItem.type = 2;
+                            infoItem.URL = "https://cdn.haonb.cc/"+item.getString("URI");
+                            infoItem.MD5 = item.getString("MD5");
+                            data.add(infoItem);
+                        }
+                        for (View otherItem : titleBarList)otherItem.setBackgroundColor(Color.WHITE);
+                        multiItem.clear();
+
+                        int Count = 0;
+                        int PageCount = 0;
+                        if (data != null){
+                            Count = data.size();
+                            PageCount = Count / 4 +1;
+                        }
+                        //
+                        for (int i=0;i<PageCount ;i++){
+                            ArrayList<EmoPanel.EmoInfo> itemInfo = new ArrayList<>();
+                            multiItem.add(itemInfo);
+                        }
+
+                        for (int i=0;i<data.size();i++){
+                            int NowPage = i / 4;
+                            ArrayList<EmoPanel.EmoInfo> cacheItem = multiItem.get(NowPage);
+                            cacheItem.add(data.get(i));
+                        }
+
+                        commonAdapter.notifyDataSetChanged();
+
+                    }catch (Exception e){
+                        Utils.ShowToastL("无法加载列表:"+e);
+                    }
+
+                });
+            }catch (Exception e){
+
+            }finally {
+                new Handler(Looper.getMainLooper()).post(()->dialog.dismiss());
+            }
+        }).start();
     }
 
     private void LoadForOnlinePicList(String ID){
