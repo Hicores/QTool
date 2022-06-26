61b4e61c0d921792eb0694384de47e6d10bdfcd0
Hicores <hicores@163.com>
2022-06-26 13:29:15 +0800

    replace HideAppEntry

--- QTool/src/main/java/cc/hicore/qtool/QQCleaner/MainCleaner/HideAppEntry.java
+++ QTool/src/main/java/cc/hicore/qtool/QQCleaner/MainCleaner/HideAppEntry.java
@@ -7,49 +7,42 @@ import android.view.ViewGroup;
 import java.lang.reflect.Method;
 
 import cc.hicore.HookItem;
+import cc.hicore.HookItemLoader.Annotations.MethodScanner;
+import cc.hicore.HookItemLoader.Annotations.UIItem;
+import cc.hicore.HookItemLoader.Annotations.VerController;
+import cc.hicore.HookItemLoader.Annotations.XPExecutor;
+import cc.hicore.HookItemLoader.Annotations.XPItem;
+import cc.hicore.HookItemLoader.bridge.BaseXPExecutor;
+import cc.hicore.HookItemLoader.bridge.MethodContainer;
+import cc.hicore.HookItemLoader.bridge.UIInfo;
 import cc.hicore.ReflectUtils.MClass;
 import cc.hicore.ReflectUtils.MMethod;
 import cc.hicore.ReflectUtils.XPBridge;
-import cc.hicore.UIItem;
 import cc.hicore.qtool.XposedInit.ItemLoader.BaseHookItem;
 import cc.hicore.qtool.XposedInit.ItemLoader.BaseUiItem;
 import cc.hicore.qtool.XposedInit.ItemLoader.HookLoader;
-
-@HookItem(isRunInAllProc = false,isDelayInit = false)
-@UIItem(name = "屏蔽下拉小程序",groupName = "主界面净化",id = "HideMainAppEntry",targetID = 2,type = 1)
-public class HideAppEntry extends BaseHookItem implements BaseUiItem {
-    boolean IsEnable;
-    @Override
-    public boolean startHook() throws Throwable {
-        XPBridge.HookBefore(getMethod(),param -> {
-            if (IsEnable)param.setResult(null);
-        });
-        return true;
+@XPItem(name = "屏蔽下拉小程序",itemType = XPItem.ITEM_Hook)
+public class HideAppEntry{
+    @VerController
+    @UIItem
+    public UIInfo getUI(){
+        UIInfo ui = new UIInfo();
+        ui.name = "屏蔽下拉小程序";
+        ui.targetID = 2;
+        ui.type = 1;
+        ui.groupName = "主界面净化";
+        return ui;
     }
-
-    @Override
-    public boolean isEnable() {
-        return IsEnable;
+    @VerController
+    @XPExecutor(methodID = "hook")
+    public BaseXPExecutor worker(){
+        return param -> param.setResult(null);
     }
-
-    @Override
-    public boolean check() {
-        return getMethod() != null;
-    }
-
-    @Override
-    public void SwitchChange(boolean IsCheck) {
-        IsEnable = IsCheck;
-        if (IsCheck) HookLoader.CallHookStart(HideAppEntry.class.getName());
-    }
-
-    @Override
-    public void ListItemClick(Context context) {
-
-    }
-    public Method getMethod(){
-        return MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.mini.api.impl.MiniAppServiceImpl"),"createMiniAppEntryManager",MClass.loadClass("com.tencent.mobileqq.mini.entry.MiniAppPullInterface"),new Class[]{
+    @VerController
+    @MethodScanner
+    public void getHookMethod(MethodContainer container){
+        container.addMethod("hook",MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.mini.api.impl.MiniAppServiceImpl"),"createMiniAppEntryManager",MClass.loadClass("com.tencent.mobileqq.mini.entry.MiniAppPullInterface"),new Class[]{
                 boolean.class, Activity.class, Object.class, Object.class, Object.class, Object.class, ViewGroup.class
-        });
+        }));
     }
 }
