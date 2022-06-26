c3813ecfa1ce90a91c48e5901ea976cd6267c26e
Hicores <hicores@163.com>
2022-06-26 13:20:11 +0800

    replace PreventShakeWindow

--- QTool/src/main/java/cc/hicore/qtool/QQCleaner/ChatCleaner/PreventShakeWindow.java
+++ QTool/src/main/java/cc/hicore/qtool/QQCleaner/ChatCleaner/PreventShakeWindow.java
@@ -5,59 +5,51 @@ import android.content.Context;
 import java.lang.reflect.Method;
 
 import cc.hicore.HookItem;
+import cc.hicore.HookItemLoader.Annotations.MethodScanner;
+import cc.hicore.HookItemLoader.Annotations.UIItem;
+import cc.hicore.HookItemLoader.Annotations.VerController;
+import cc.hicore.HookItemLoader.Annotations.XPExecutor;
+import cc.hicore.HookItemLoader.Annotations.XPItem;
+import cc.hicore.HookItemLoader.bridge.BaseXPExecutor;
+import cc.hicore.HookItemLoader.bridge.MethodContainer;
+import cc.hicore.HookItemLoader.bridge.UIInfo;
 import cc.hicore.ReflectUtils.MClass;
 import cc.hicore.ReflectUtils.MField;
 import cc.hicore.ReflectUtils.MMethod;
 import cc.hicore.ReflectUtils.XPBridge;
-import cc.hicore.UIItem;
 import cc.hicore.qtool.XposedInit.ItemLoader.BaseHookItem;
 import cc.hicore.qtool.XposedInit.ItemLoader.BaseUiItem;
 import cc.hicore.qtool.XposedInit.ItemLoader.HookLoader;
-
-@HookItem(isRunInAllProc = false,isDelayInit = false)
-@UIItem(name = "屏蔽窗口抖动",targetID = 2,groupName = "聊天净化",type = 1,id = "PreventShakeWindow")
-public class PreventShakeWindow extends BaseHookItem implements BaseUiItem {
-    boolean IsEnable;
-    @Override
-    public boolean startHook() throws Throwable {
-        XPBridge.HookBefore(getMethod(),param -> {
-            if (IsEnable){
-                Object ChatMsg = param.args[2];
-                if (ChatMsg.getClass().getSimpleName().equals("MessageForShakeWindow")){
-                    MField.SetField(ChatMsg,"isread",true);
-                }
-
-            }
-        });
-        return true;
+@XPItem(name = "屏蔽窗口抖动",itemType = XPItem.ITEM_Hook)
+public class PreventShakeWindow{
+    @VerController
+    @UIItem
+    public UIInfo getUI(){
+        UIInfo ui = new UIInfo();
+        ui.name = "屏蔽窗口抖动";
+        ui.groupName = "聊天净化";
+        ui.type = 1;
+        ui.targetID = 2;
+        return ui;
     }
-
-    @Override
-    public boolean isEnable() {
-        return IsEnable;
-    }
-
-    @Override
-    public boolean check() {
-        return getMethod() != null;
-    }
-
-    @Override
-    public void SwitchChange(boolean IsCheck) {
-        IsEnable  =IsCheck;
-        if (IsEnable) HookLoader.CallHookStart(PreventShakeWindow.class.getName());
-    }
-
-    @Override
-    public void ListItemClick(Context context) {
-
-    }
-    public Method getMethod(){
-        return MMethod.FindMethod("com.tencent.mobileqq.troop.data.TroopAndDiscMsgProxy",null, void.class,new Class[]{
+    @VerController
+    @MethodScanner
+    public void getHookMethod(MethodContainer container){
+        container.addMethod("hook",MMethod.FindMethod("com.tencent.mobileqq.troop.data.TroopAndDiscMsgProxy",null, void.class,new Class[]{
                 String.class,
                 int.class,
                 MClass.loadClass("com.tencent.mobileqq.data.MessageRecord"),
                 boolean.class
-        });
+        }));
+    }
+    @VerController
+    @XPExecutor(methodID = "hook")
+    public BaseXPExecutor worker() {
+        return param -> {
+            Object ChatMsg = param.args[2];
+            if (ChatMsg.getClass().getSimpleName().equals("MessageForShakeWindow")){
+                MField.SetField(ChatMsg,"isread",true);
+            }
+        };
     }
 }
