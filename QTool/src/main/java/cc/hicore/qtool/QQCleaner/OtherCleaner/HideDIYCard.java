41aa7ffbe310cf0b86428d777bffd26866c47ae1
Hicores <hicores@163.com>
2022-06-27 22:16:54 +0800

    replace HideDIYCard

--- QTool/src/main/java/cc/hicore/qtool/QQCleaner/OtherCleaner/HideDIYCard.java
+++ QTool/src/main/java/cc/hicore/qtool/QQCleaner/OtherCleaner/HideDIYCard.java
@@ -5,55 +5,48 @@ import android.content.Context;
 import java.lang.reflect.Method;
 
 import cc.hicore.HookItem;
+import cc.hicore.HookItemLoader.Annotations.MethodScanner;
+import cc.hicore.HookItemLoader.Annotations.UIItem;
+import cc.hicore.HookItemLoader.Annotations.VerController;
+import cc.hicore.HookItemLoader.Annotations.XPExecutor;
+import cc.hicore.HookItemLoader.Annotations.XPItem;
+import cc.hicore.HookItemLoader.bridge.BaseXPExecutor;
+import cc.hicore.HookItemLoader.bridge.MethodContainer;
+import cc.hicore.HookItemLoader.bridge.UIInfo;
 import cc.hicore.ReflectUtils.MClass;
 import cc.hicore.ReflectUtils.MField;
 import cc.hicore.ReflectUtils.MMethod;
 import cc.hicore.ReflectUtils.XPBridge;
-import cc.hicore.UIItem;
 import cc.hicore.qtool.XposedInit.ItemLoader.BaseHookItem;
 import cc.hicore.qtool.XposedInit.ItemLoader.BaseUiItem;
 import cc.hicore.qtool.XposedInit.ItemLoader.HookLoader;
-
-@HookItem(isDelayInit = false,isRunInAllProc = false)
-@UIItem(name = "屏蔽DIY名片",targetID = 2,groupName = "其他净化",type = 1,id = "HideDIYCard")
-public class HideDIYCard extends BaseHookItem implements BaseUiItem {
-    boolean IsEnable;
-    @Override
-    public boolean startHook() throws Throwable {
-        XPBridge.HookAfter(getMethod(),param -> {
-            if (IsEnable){
-                MField.SetField(param.args[0],"lCurrentStyleId",(long)0);
-            }
-        });
-        return true;
+@XPItem(name = "屏蔽DIY名片",itemType = XPItem.ITEM_Hook)
+public class HideDIYCard{
+    @VerController
+    @UIItem
+    public UIInfo getUI(){
+        UIInfo ui = new UIInfo();
+        ui.name = "屏蔽DIY名片";
+        ui.targetID = 2;
+        ui.type = 1;
+        ui.groupName = "其他净化";
+        return ui;
     }
-
-    @Override
-    public boolean isEnable() {
-        return IsEnable;
-    }
-
-    @Override
-    public boolean check() {
-        return getMethod() != null;
-    }
-
-    @Override
-    public void SwitchChange(boolean IsCheck) {
-        IsEnable = IsCheck;
-        if (IsCheck) HookLoader.CallHookStart(HideDIYCard.class.getName());
-    }
-
-    @Override
-    public void ListItemClick(Context context) {
-
-    }
-    public Method getMethod(){
-        return MMethod.FindMethod("com.tencent.mobileqq.profilecard.processor.TempProfileBusinessProcessor","updateCardTemplate", void.class,new Class[]{
+    @VerController
+    @MethodScanner
+    public void getHookMethod(MethodContainer container){
+        container.addMethod("hook",MMethod.FindMethod("com.tencent.mobileqq.profilecard.processor.TempProfileBusinessProcessor","updateCardTemplate", void.class,new Class[]{
                 MClass.loadClass("com.tencent.mobileqq.data.Card"),
                 String.class,
                 MClass.loadClass("SummaryCardTaf.SSummaryCardRsp")
-        });
+        }));
+    }
+    @VerController
+    @XPExecutor(methodID = "hook",period = XPExecutor.After)
+    public BaseXPExecutor worker(){
+        return param -> {
+            MField.SetField(param.args[0],"lCurrentStyleId",(long)0);
+        };
     }
 
 }
