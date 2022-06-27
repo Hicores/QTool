e47706251e2be44fec7d89dcd63e347882ecd3c6
Hicores <hicores@163.com>
2022-06-27 23:35:16 +0800

    replace SavedToXml

--- QTool/src/main/java/cc/hicore/qtool/XPWork/LittleHook/SavedToXml.java
+++ QTool/src/main/java/cc/hicore/qtool/XPWork/LittleHook/SavedToXml.java
@@ -1,6 +1,9 @@
 package cc.hicore.qtool.XPWork.LittleHook;
 
 import cc.hicore.HookItem;
+import cc.hicore.HookItemLoader.Annotations.CommonExecutor;
+import cc.hicore.HookItemLoader.Annotations.VerController;
+import cc.hicore.HookItemLoader.Annotations.XPItem;
 import cc.hicore.ReflectUtils.Classes;
 import cc.hicore.ReflectUtils.MClass;
 import cc.hicore.ReflectUtils.MMethod;
@@ -8,10 +11,11 @@ import cc.hicore.qtool.XposedInit.ItemLoader.BaseHookItem;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
 
-@HookItem(isRunInAllProc = false, isDelayInit = false)
-public class SavedToXml extends BaseHookItem {
-    @Override
-    public boolean startHook() throws Throwable {
+@XPItem(name = "XP_Save",itemType = XPItem.ITEM_Hook)
+public class SavedToXml{
+    @VerController
+    @CommonExecutor
+    public void execute(){
         XposedHelpers.findAndHookMethod(MClass.loadClass("com.tencent.mobileqq.data.MessageForStarLeague"), "decodeMsgFromXmlBuff", Classes.QQAppinterFace(), int.class, long.class, byte[].class, int.class, new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
@@ -20,16 +24,5 @@ public class SavedToXml extends BaseHookItem {
                 MMethod.CallMethodParams(param.getResult(), "saveExtInfoToExtStr", void.class, "SavedXml", new String(arr));
             }
         });
-        return true;
-    }
-
-    @Override
-    public boolean isEnable() {
-        return true;
-    }
-
-    @Override
-    public boolean check() {
-        return true;
     }
 }
