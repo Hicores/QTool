43c579549915ee37455e472eacf6a6aba5a50f81
Hicores <hicores@163.com>
2022-06-27 23:39:12 +0800

    replace ShowFullMessageCount

--- QTool/src/main/java/cc/hicore/qtool/XPWork/LittleHook/ShowFullMessageCount.java
+++ QTool/src/main/java/cc/hicore/qtool/XPWork/LittleHook/ShowFullMessageCount.java
@@ -7,9 +7,17 @@ import android.widget.TextView;
 import java.lang.reflect.Method;
 
 import cc.hicore.HookItem;
+import cc.hicore.HookItemLoader.Annotations.MethodScanner;
+import cc.hicore.HookItemLoader.Annotations.UIItem;
+import cc.hicore.HookItemLoader.Annotations.VerController;
+import cc.hicore.HookItemLoader.Annotations.XPExecutor;
+import cc.hicore.HookItemLoader.Annotations.XPItem;
+import cc.hicore.HookItemLoader.bridge.BaseXPExecutor;
+import cc.hicore.HookItemLoader.bridge.MethodContainer;
+import cc.hicore.HookItemLoader.bridge.QQVersion;
+import cc.hicore.HookItemLoader.bridge.UIInfo;
 import cc.hicore.ReflectUtils.MClass;
 import cc.hicore.ReflectUtils.MMethod;
-import cc.hicore.UIItem;
 import cc.hicore.Utils.Utils;
 import cc.hicore.qtool.HookEnv;
 import cc.hicore.qtool.XposedInit.HostInfo;
@@ -18,64 +26,43 @@ import cc.hicore.qtool.XposedInit.ItemLoader.BaseUiItem;
 import cc.hicore.qtool.XposedInit.ItemLoader.HookLoader;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
-
-@HookItem(isDelayInit = false,isRunInAllProc = false)
-@UIItem(name = "显示完整消息数量",targetID = 1,groupName = "功能辅助",id = "ShowFullMessageCount",type = 1)
-public class ShowFullMessageCount extends BaseHookItem implements BaseUiItem {
-    boolean IsEnable;
-    @Override
-    public boolean startHook() throws Throwable {
-        XposedBridge.hookMethod(getMethod(), new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                super.beforeHookedMethod(param);
-                if (IsEnable){
-                    param.args[4]=Integer.MAX_VALUE;
-                }
-            }
-
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                super.afterHookedMethod(param);
-                if (IsEnable && HostInfo.getVerCode() > 5570){
-                    int Type = (int) param.args[1];
-                    if(Type == 4 || Type == 7 || Type == 9|| Type == 3) {
-                        TextView t = (TextView) param.args[0];
-                        int Count = (int) param.args[2];
-                        String str = ""+Count;
-                        ViewGroup.LayoutParams params = t.getLayoutParams();
-                        params.width = Utils.dip2px(HookEnv.AppContext,9+7*str.length());
-                        t.setLayoutParams(params);
-                    }
-                }
-            }
-        });
-        return true;
+@XPItem(name = "显示完整消息数量",itemType = XPItem.ITEM_Hook)
+public class ShowFullMessageCount {
+    @VerController
+    @UIItem
+    public UIInfo getUI(){
+        UIInfo ui = new UIInfo();
+        ui.name = "显示完整消息数量";
+        ui.groupName = "功能辅助";
+        ui.targetID = 1;
+        ui.type = 1;
+        return ui;
     }
-
-    @Override
-    public boolean isEnable() {
-        return IsEnable;
-    }
-
-    @Override
-    public boolean check() {
-        return getMethod() != null;
-    }
-
-    @Override
-    public void SwitchChange(boolean IsCheck) {
-        IsEnable = IsCheck;
-        if (IsCheck) HookLoader.CallHookStart(ShowFullMessageCount.class.getName());
-    }
-
-    @Override
-    public void ListItemClick(Context context) {
-
-    }
-    public Method getMethod(){
-        return MMethod.FindMethod(MClass.loadClass("com.tencent.widget.CustomWidgetUtil"),null,void.class,new Class[]{
+    @VerController
+    @MethodScanner
+    public void getHookMethod(MethodContainer container){
+        container.addMethod("hook",MMethod.FindMethod(MClass.loadClass("com.tencent.widget.CustomWidgetUtil"),null,void.class,new Class[]{
                 TextView.class, int.class, int.class, int.class, int.class, String.class
-        });
+        }));
+    }
+    @VerController
+    @XPExecutor(methodID = "hook")
+    public BaseXPExecutor before(){
+        return param -> param.args[4]=Integer.MAX_VALUE;
+    }
+    @VerController(targetVer = QQVersion.QQ_8_8_11)
+    @XPExecutor(methodID = "hook")
+    public BaseXPExecutor after(){
+        return param -> {
+            int Type = (int) param.args[1];
+            if(Type == 4 || Type == 7 || Type == 9|| Type == 3) {
+                TextView t = (TextView) param.args[0];
+                int Count = (int) param.args[2];
+                String str = ""+Count;
+                ViewGroup.LayoutParams params = t.getLayoutParams();
+                params.width = Utils.dip2px(HookEnv.AppContext,9+7*str.length());
+                t.setLayoutParams(params);
+            }
+        };
     }
 }
