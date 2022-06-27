17cf0b19b6ee5f49f33676ad18c5b18b9cec1fde
Hicores <hicores@163.com>
2022-06-27 23:05:47 +0800

    replace ToggleKiller

--- QTool/src/main/java/cc/hicore/qtool/ServerKiller/ToggleKiller.java
+++ QTool/src/main/java/cc/hicore/qtool/ServerKiller/ToggleKiller.java
@@ -6,49 +6,42 @@ import android.os.Message;
 import java.lang.reflect.Method;
 
 import cc.hicore.HookItem;
+import cc.hicore.HookItemLoader.Annotations.MethodScanner;
+import cc.hicore.HookItemLoader.Annotations.UIItem;
+import cc.hicore.HookItemLoader.Annotations.VerController;
+import cc.hicore.HookItemLoader.Annotations.XPExecutor;
+import cc.hicore.HookItemLoader.Annotations.XPItem;
+import cc.hicore.HookItemLoader.bridge.BaseXPExecutor;
+import cc.hicore.HookItemLoader.bridge.MethodContainer;
+import cc.hicore.HookItemLoader.bridge.UIInfo;
 import cc.hicore.ReflectUtils.MClass;
 import cc.hicore.ReflectUtils.MMethod;
 import cc.hicore.ReflectUtils.XPBridge;
-import cc.hicore.UIItem;
 import cc.hicore.qtool.XposedInit.ItemLoader.BaseHookItem;
 import cc.hicore.qtool.XposedInit.ItemLoader.BaseUiItem;
 import cc.hicore.qtool.XposedInit.ItemLoader.HookLoader;
-
-@HookItem(isDelayInit = false,isRunInAllProc = true)
-@UIItem(name = "禁用Toggle",groupName = "服务调节",targetID = 4,type = 1,id = "ToggleKiller")
-public class ToggleKiller extends BaseHookItem implements BaseUiItem {
-    boolean IsEnable;
-    @Override
-    public boolean startHook() throws Throwable {
-        XPBridge.HookBefore(getMethod(),param -> {
-            if (IsEnable)param.setResult(null);
-        });
-        return true;
+@XPItem(name = "禁用Toggle",itemType = XPItem.ITEM_Hook,proc = XPItem.PROC_ALL)
+public class ToggleKiller{
+    @VerController
+    @UIItem
+    public UIInfo getUI(){
+        UIInfo ui = new UIInfo();
+        ui.name = "禁用Toggle";
+        ui.groupName = "服务调节";
+        ui.targetID = 4;
+        ui.type = 1;
+        return ui;
     }
-
-    @Override
-    public boolean isEnable() {
-        return IsEnable;
-    }
-
-    @Override
-    public boolean check() {
-        return getMethod() != null;
-    }
-
-    @Override
-    public void SwitchChange(boolean IsCheck) {
-        IsEnable = IsCheck;
-        if (IsEnable) HookLoader.CallHookStart(ToggleKiller.class.getName());
-    }
-
-    @Override
-    public void ListItemClick(Context context) {
-
-    }
-    public Method getMethod(){
-        return MMethod.FindMethod(MClass.loadClass("com.tencent.ft.op.ToggleDispatcher$11"),"handleMessage",void.class,new Class[]{
+    @VerController
+    @MethodScanner
+    public void getHookMethod(MethodContainer container){
+        container.addMethod("hook",MMethod.FindMethod(MClass.loadClass("com.tencent.ft.op.ToggleDispatcher$11"),"handleMessage",void.class,new Class[]{
                 Message.class
-        });
+        }));
+    }
+    @VerController
+    @XPExecutor(methodID = "hook")
+    public BaseXPExecutor worker(){
+        return param -> param.setResult(null);
     }
 }
