41e8eea7ca68ebcfb352fd33a7257e492817cfc4
Hicores <hicores@163.com>
2022-06-27 23:44:22 +0800

    replace UnlockWebLock

--- QTool/src/main/java/cc/hicore/qtool/XPWork/LittleHook/UnlockWebLock.java
+++ QTool/src/main/java/cc/hicore/qtool/XPWork/LittleHook/UnlockWebLock.java
@@ -6,59 +6,53 @@ import java.lang.reflect.Method;
 import java.net.URLDecoder;
 
 import cc.hicore.HookItem;
+import cc.hicore.HookItemLoader.Annotations.MethodScanner;
+import cc.hicore.HookItemLoader.Annotations.UIItem;
+import cc.hicore.HookItemLoader.Annotations.VerController;
+import cc.hicore.HookItemLoader.Annotations.XPExecutor;
+import cc.hicore.HookItemLoader.Annotations.XPItem;
+import cc.hicore.HookItemLoader.bridge.BaseXPExecutor;
+import cc.hicore.HookItemLoader.bridge.MethodContainer;
+import cc.hicore.HookItemLoader.bridge.UIInfo;
 import cc.hicore.ReflectUtils.MClass;
 import cc.hicore.ReflectUtils.MMethod;
 import cc.hicore.ReflectUtils.XPBridge;
-import cc.hicore.UIItem;
 import cc.hicore.qtool.HookEnv;
 import cc.hicore.qtool.XposedInit.ItemLoader.BaseHookItem;
 import cc.hicore.qtool.XposedInit.ItemLoader.BaseUiItem;
 import cc.hicore.qtool.XposedInit.ItemLoader.HookLoader;
-
-@HookItem(isDelayInit = false,isRunInAllProc = true)
-@UIItem(name = "解除风险网址拦截",groupName = "功能辅助",targetID = 1,type = 1,id = "UnlockWebLock")
-public class UnlockWebLock extends BaseHookItem implements BaseUiItem {
-    @Override
-    public boolean startHook() throws Throwable {
-        XPBridge.HookBefore(getMethod(),param -> {
-            if (isEnable()){
-                String loadUrl = (String) param.args[0];
-                if(loadUrl.startsWith("https://c.pc.qq.com/middlem.html?") || loadUrl.startsWith("https://c.pc.qq.com/index.html?")) {
-                    String RedictUrl = GetStringMiddle(loadUrl,"url=","&");
-                    if(RedictUrl!=null) {
-                        String SourceUrl = URLDecoder.decode(RedictUrl);
-                        param.args[0] = SourceUrl;
-                    }
+@XPItem(name = "解除风险网址拦截",itemType = XPItem.ITEM_Hook,proc = XPItem.PROC_ALL)
+public class UnlockWebLock{
+    @VerController
+    @UIItem
+    public UIInfo getUI(){
+        UIInfo ui = new UIInfo();
+        ui.name = "解除风险网址拦截";
+        ui.groupName = "功能辅助";
+        ui.targetID = 1;
+        ui.type = 1;
+        return ui;
+    }
+    @VerController
+    @MethodScanner
+    public void getHookMethod(MethodContainer container){
+        container.addMethod("hook",MMethod.FindMethod(MClass.loadClass("com.tencent.smtt.sdk.WebView"), "loadUrl",void.class,new Class[]{
+                String.class
+        }));
+    }
+    @VerController
+    @XPExecutor(methodID = "hook")
+    public BaseXPExecutor worker(){
+        return param -> {
+            String loadUrl = (String) param.args[0];
+            if(loadUrl.startsWith("https://c.pc.qq.com/middlem.html?") || loadUrl.startsWith("https://c.pc.qq.com/index.html?")) {
+                String RedictUrl = GetStringMiddle(loadUrl,"url=","&");
+                if(RedictUrl!=null) {
+                    String SourceUrl = URLDecoder.decode(RedictUrl);
+                    param.args[0] = SourceUrl;
                 }
             }
-        });
-        return true;
-    }
-
-    @Override
-    public boolean isEnable() {
-        return  HookEnv.Config.getBoolean("Set","UnlockWebLock",false);
-    }
-
-    @Override
-    public boolean check() {
-        return getMethod() != null;
-    }
-
-    @Override
-    public void SwitchChange(boolean IsCheck) {
-        HookEnv.Config.setBoolean("Set","UnlockWebLock",IsCheck);
-        HookLoader.CallHookStart(UnlockWebLock.class.getName());
-    }
-
-    @Override
-    public void ListItemClick(Context context) {
-
-    }
-    public Method getMethod(){
-        return MMethod.FindMethod(MClass.loadClass("com.tencent.smtt.sdk.WebView"), "loadUrl",void.class,new Class[]{
-                String.class
-        });
+        };
     }
     private static String GetStringMiddle(String str,String before,String after) {
         int index1 = str.indexOf(before);
