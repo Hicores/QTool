86e9c29e435f8d28e089c61d8e2cc067360df54d
Hicores <hicores@163.com>
2022-06-27 23:49:15 +0800

    replace BaseGuildMsgProxy

--- QTool/src/main/java/cc/hicore/qtool/XPWork/QQProxy/BaseGuildMsgProxy.java
+++ QTool/src/main/java/cc/hicore/qtool/XPWork/QQProxy/BaseGuildMsgProxy.java
@@ -3,6 +3,12 @@ package cc.hicore.qtool.XPWork.QQProxy;
 import java.lang.reflect.Method;
 
 import cc.hicore.HookItem;
+import cc.hicore.HookItemLoader.Annotations.MethodScanner;
+import cc.hicore.HookItemLoader.Annotations.VerController;
+import cc.hicore.HookItemLoader.Annotations.XPExecutor;
+import cc.hicore.HookItemLoader.Annotations.XPItem;
+import cc.hicore.HookItemLoader.bridge.BaseXPExecutor;
+import cc.hicore.HookItemLoader.bridge.MethodContainer;
 import cc.hicore.ReflectUtils.Classes;
 import cc.hicore.ReflectUtils.MClass;
 import cc.hicore.ReflectUtils.MMethod;
@@ -10,47 +16,29 @@ import cc.hicore.ReflectUtils.XPBridge;
 import cc.hicore.qtool.JavaPlugin.Controller.PluginMessageProcessor;
 import cc.hicore.qtool.XposedInit.ItemLoader.BaseHookItem;
 
-@HookItem(isRunInAllProc = false, isDelayInit = false)
-public class BaseGuildMsgProxy extends BaseHookItem {
-    private static final String TAG = "BaseGuildMsgProxy";
-
-    @Override
-    public String getTag() {
-        return TAG;
+@XPItem(name = "Proxy_Guild_Msg",itemType = XPItem.ITEM_Hook)
+public class BaseGuildMsgProxy{
+    @VerController
+    @MethodScanner
+    public void getHookMethod(MethodContainer container){
+        container.addMethod("hook_1",MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.guild.message.GuildOnlineMessageProcessor"), null,
+                void.class, new Class[]{Classes.MessageRecord()}));
+        container.addMethod("hook_2",MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.guild.message.api.impl.GuildMessageUtilsApiImpl"), "handleSelfSendMsg",
+                void.class, new Class[]{Classes.AppInterface(), Classes.MessageRecord(), Classes.MessageRecord(), int.class}));
     }
-
-    @Override
-    public boolean startHook() {
-        Method[] m = getMethod();
-        XPBridge.HookBefore(m[0], param -> {
-            PluginMessageProcessor.submit(() -> PluginMessageProcessor.onMessage(param.args[0]));
-        });
-        XPBridge.HookBefore(m[1], param -> {
+    @VerController
+    @XPExecutor(methodID = "hook_1")
+    public BaseXPExecutor worker_1(){
+        return param -> PluginMessageProcessor.submit(() -> PluginMessageProcessor.onMessage(param.args[0]));
+    }
+    @VerController
+    @XPExecutor(methodID = "hook_2")
+    public BaseXPExecutor worker_2(){
+        return param -> {
             if (!param.args[1].getClass().getSimpleName().equals("MessageRecord")) {
                 PluginMessageProcessor.submit(() -> PluginMessageProcessor.onMessage(param.args[1]));
 
             }
-        });
-        return true;
-    }
-
-    @Override
-    public boolean isEnable() {
-        return true;
-    }
-
-    @Override
-    public boolean check() {
-        Method[] m = getMethod();
-        return m[0] != null & m[1] != null;
-    }
-
-    public Method[] getMethod() {
-        Method[] m = new Method[2];
-        m[0] = MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.guild.message.GuildOnlineMessageProcessor"), null,
-                void.class, new Class[]{Classes.MessageRecord()});
-        m[1] = MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.guild.message.api.impl.GuildMessageUtilsApiImpl"), "handleSelfSendMsg",
-                void.class, new Class[]{Classes.AppInterface(), Classes.MessageRecord(), Classes.MessageRecord(), int.class});
-        return m;
+        };
     }
 }
