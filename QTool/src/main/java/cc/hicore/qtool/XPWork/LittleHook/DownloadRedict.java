f44804795f05d412a55b6e8685bf45fe13cec657
Hicores <hicores@163.com>
2022-06-27 23:23:41 +0800

    replace DownloadRedict

--- QTool/src/main/java/cc/hicore/qtool/XPWork/LittleHook/DownloadRedict.java
+++ QTool/src/main/java/cc/hicore/qtool/XPWork/LittleHook/DownloadRedict.java
@@ -12,55 +12,62 @@ import java.io.File;
 import java.lang.reflect.Method;
 
 import cc.hicore.HookItem;
+import cc.hicore.HookItemLoader.Annotations.MethodScanner;
+import cc.hicore.HookItemLoader.Annotations.UIClick;
+import cc.hicore.HookItemLoader.Annotations.UIItem;
+import cc.hicore.HookItemLoader.Annotations.VerController;
+import cc.hicore.HookItemLoader.Annotations.XPExecutor;
+import cc.hicore.HookItemLoader.Annotations.XPItem;
+import cc.hicore.HookItemLoader.bridge.BaseXPExecutor;
+import cc.hicore.HookItemLoader.bridge.MethodContainer;
+import cc.hicore.HookItemLoader.bridge.UIInfo;
 import cc.hicore.ReflectUtils.MClass;
 import cc.hicore.ReflectUtils.MMethod;
 import cc.hicore.ReflectUtils.XPBridge;
-import cc.hicore.UIItem;
 import cc.hicore.Utils.Utils;
 import cc.hicore.qtool.HookEnv;
 import cc.hicore.qtool.XposedInit.ItemLoader.BaseHookItem;
 import cc.hicore.qtool.XposedInit.ItemLoader.BaseUiItem;
-@HookItem(isDelayInit = false,isRunInAllProc = true)
-@UIItem(name = "下载重定向",groupName = "功能辅助",targetID = 1,type = 2,id = "DownloadRedict")
-public class DownloadRedict extends BaseHookItem implements BaseUiItem {
-    @Override
-    public boolean startHook() throws Throwable {
-        XPBridge.HookAfter(getMethod(),param -> {
+@XPItem(name = "下载重定向",itemType = XPItem.ITEM_Hook,proc = XPItem.PROC_ALL)
+public class DownloadRedict{
+    @VerController
+    @UIItem
+    public UIInfo getUI(){
+        UIInfo ui = new UIInfo();
+        ui.name = "下载重定向";
+        ui.groupName = "功能辅助";
+        ui.targetID = 1;
+        ui.type = 1;
+        return ui;
+    }
+    @VerController
+    @MethodScanner
+    public void getHookMethod(MethodContainer container){
+        container.addMethod("hook",MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.vfs.VFSAssistantUtils"),"getSDKPrivatePath",String.class,new Class[]{
+                String.class
+        }));
+    }
+    @VerController
+    @XPExecutor(methodID = "hook",period = XPExecutor.After)
+    public BaseXPExecutor worker(){
+        return param -> {
             String Path = (String) param.args[0];
             String Result = (String) param.getResult();
-            if (isEnable()){
-                if(Result.contains("/Tencent/QQfile_recv/")) {
-                    if(new File(Result).exists() && new File(Result).isFile())return;//如果下载的文件已经存在则不替换,防止与QQ文件数据库出错而导致无法下载的问题
-                    String End = Path.substring(Path.lastIndexOf("/Tencent/QQfile_recv/")+"/Tencent/QQfile_recv/".length());
-                    String Start = HookEnv.Config.getString("Set","DownloadRedictPath","");
+            if(Result.contains("/Tencent/QQfile_recv/")) {
+                if(new File(Result).exists() && new File(Result).isFile())return;//如果下载的文件已经存在则不替换,防止与QQ文件数据库出错而导致无法下载的问题
+                String End = Path.substring(Path.lastIndexOf("/Tencent/QQfile_recv/")+"/Tencent/QQfile_recv/".length());
+                String Start = HookEnv.Config.getString("Set","DownloadRedictPath","");
 
-                    if(TextUtils.isEmpty(Start))Start = Environment.getExternalStorageDirectory()+"/Download/MobileQQ/";
-                    if(!Start.endsWith("/"))Start = Start + "/";
-                    param.setResult(Start+End);
-                    return;
-                }
+                if(TextUtils.isEmpty(Start))Start = Environment.getExternalStorageDirectory()+"/Download/MobileQQ/";
+                if(!Start.endsWith("/"))Start = Start + "/";
+                param.setResult(Start+End);
+                return;
             }
-        });
-        return true;
+        };
     }
-
-    @Override
-    public boolean isEnable() {
-        return HookEnv.Config.getBoolean("Set","DownloadRedictOpen",false);
-    }
-
-    @Override
-    public boolean check() {
-        return getMethod() != null;
-    }
-
-    @Override
-    public void SwitchChange(boolean IsCheck) {
-
-    }
-
-    @Override
-    public void ListItemClick(Context context) {
+    @VerController
+    @UIClick
+    public void uiClick(Context context){
         LinearLayout mRoot = new LinearLayout(context);
         mRoot.setOrientation(LinearLayout.VERTICAL);
 
@@ -102,10 +109,5 @@ public class DownloadRedict extends BaseHookItem implements BaseUiItem {
         File f = new File(Path);
         f.mkdirs();
         return f.exists() && f.isDirectory();
-    }
-    public Method getMethod(){
-        return MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.vfs.VFSAssistantUtils"),"getSDKPrivatePath",String.class,new Class[]{
-                String.class
-        });
     }
 }
