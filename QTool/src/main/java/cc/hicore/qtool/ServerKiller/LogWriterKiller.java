03fa0344be43bf9934cad03bfefb95e418521f41
Hicores <hicores@163.com>
2022-06-27 23:02:46 +0800

    replace LogWriterKiller

--- QTool/src/main/java/cc/hicore/qtool/ServerKiller/LogWriterKiller.java
+++ QTool/src/main/java/cc/hicore/qtool/ServerKiller/LogWriterKiller.java
@@ -6,62 +6,64 @@ import java.lang.reflect.Method;
 import java.util.List;
 
 import cc.hicore.HookItem;
+import cc.hicore.HookItemLoader.Annotations.MethodScanner;
+import cc.hicore.HookItemLoader.Annotations.UIItem;
+import cc.hicore.HookItemLoader.Annotations.VerController;
+import cc.hicore.HookItemLoader.Annotations.XPExecutor;
+import cc.hicore.HookItemLoader.Annotations.XPItem;
+import cc.hicore.HookItemLoader.bridge.BaseXPExecutor;
+import cc.hicore.HookItemLoader.bridge.MethodContainer;
+import cc.hicore.HookItemLoader.bridge.UIInfo;
 import cc.hicore.ReflectUtils.MClass;
 import cc.hicore.ReflectUtils.MMethod;
 import cc.hicore.ReflectUtils.XPBridge;
-import cc.hicore.UIItem;
 import cc.hicore.qtool.XposedInit.ItemLoader.BaseHookItem;
 import cc.hicore.qtool.XposedInit.ItemLoader.BaseUiItem;
 import cc.hicore.qtool.XposedInit.ItemLoader.HookLoader;
-
-@HookItem(isDelayInit = false,isRunInAllProc = true)
-@UIItem(name = "禁用LoggerWriter",groupName = "服务调节",targetID = 4,type = 1,id = "LogWriterKiller")
-public class LogWriterKiller extends BaseHookItem implements BaseUiItem {
-    boolean IsEnable;
-    @Override
-    public boolean startHook() throws Throwable {
-        Method[] m = getMethod();
-        XPBridge.HookBefore(m[0],param -> param.setResult(null));
-        XPBridge.HookBefore(m[1],param -> param.setResult(true));
-        XPBridge.HookBefore(m[2],param -> param.setResult(null));
-        XPBridge.HookBefore(m[3],param -> param.setResult(null));
-        return true;
+@XPItem(name = "禁用LoggerWriter",itemType = XPItem.ITEM_Hook,proc = XPItem.PROC_ALL)
+public class LogWriterKiller{
+    @VerController
+    @UIItem
+    public UIInfo getUI(){
+        UIInfo ui = new UIInfo();
+        ui.name = "禁用LoggerWriter";
+        ui.groupName = "服务调节";
+        ui.targetID = 4;
+        ui.type = 1;
+        return ui;
     }
-
-    @Override
-    public boolean isEnable() {
-        return IsEnable;
-    }
-
-    @Override
-    public boolean check() {
-        Method[] m = getMethod();
-        return m[0] != null && m[1] != null;
-    }
-
-    @Override
-    public void SwitchChange(boolean IsCheck) {
-        IsEnable = IsCheck;
-        if (IsCheck) HookLoader.CallHookStart(LogWriterKiller.class.getName());
-    }
-
-    @Override
-    public void ListItemClick(Context context) {
-
-    }
-    public Method[] getMethod(){
-        Method[] m = new Method[4];
-        m[0] = MMethod.FindMethod(MClass.loadClass("com.tencent.qphone.base.util.QLogItemManager"),"addLogItem",void.class,new Class[]{
+    @VerController
+    @MethodScanner
+    public void getHookMethod(MethodContainer container){
+        container.addMethod("hook_1",MMethod.FindMethod(MClass.loadClass("com.tencent.qphone.base.util.QLogItemManager"),"addLogItem",void.class,new Class[]{
                 MClass.loadClass("com.tencent.qphone.base.util.QLogItem")
-        });
-        m[1] = MMethod.FindMethod(MClass.loadClass("com.tencent.qphone.base.util.LogWriterManager"),"writeLogItems",boolean.class,new Class[]{
+        }));
+        container.addMethod("hook_2",MMethod.FindMethod(MClass.loadClass("com.tencent.qphone.base.util.LogWriterManager"),"writeLogItems",boolean.class,new Class[]{
                 List.class
-        });
-        m[2] = MMethod.FindMethod(MClass.loadClass("com.tencent.qphone.base.util.QLogItemManager"),"init",void.class,new Class[]{
+        }));
+        container.addMethod("hook_3",MMethod.FindMethod(MClass.loadClass("com.tencent.qphone.base.util.QLogItemManager"),"init",void.class,new Class[]{
                 long.class
-        });
-        m[3] = MMethod.FindMethod(MClass.loadClass("com.tencent.qphone.base.util.QLogItemManager$WriteHandler"),"tryInit",void.class,new Class[0]);
-
-        return m;
+        }));
+        container.addMethod("hook_4",MMethod.FindMethod(MClass.loadClass("com.tencent.qphone.base.util.QLogItemManager$WriteHandler"),"tryInit",void.class,new Class[0]));
+    }
+    @VerController
+    @XPExecutor(methodID = "hook_1")
+    public BaseXPExecutor worker_1(){
+        return param -> param.setResult(null);
+    }
+    @VerController
+    @XPExecutor(methodID = "hook_2")
+    public BaseXPExecutor worker_2(){
+        return param -> param.setResult(true);
+    }
+    @VerController
+    @XPExecutor(methodID = "hook_3")
+    public BaseXPExecutor worker_3(){
+        return param -> param.setResult(null);
+    }
+    @VerController
+    @XPExecutor(methodID = "hook_4")
+    public BaseXPExecutor worker_4(){
+        return param -> param.setResult(null);
     }
 }
