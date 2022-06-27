23774e99341557d8bc23f4147e97186ee98225a6
Hicores <hicores@163.com>
2022-06-27 22:51:05 +0800

    replace FileFailureRename

--- QTool/src/main/java/cc/hicore/qtool/QQGroupHelper/FileHelper/FileFailureRename.java
+++ QTool/src/main/java/cc/hicore/qtool/QQGroupHelper/FileHelper/FileFailureRename.java
@@ -13,28 +13,48 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 import cc.hicore.HookItem;
+import cc.hicore.HookItemLoader.Annotations.MethodScanner;
+import cc.hicore.HookItemLoader.Annotations.UIItem;
+import cc.hicore.HookItemLoader.Annotations.VerController;
+import cc.hicore.HookItemLoader.Annotations.XPExecutor;
+import cc.hicore.HookItemLoader.Annotations.XPItem;
+import cc.hicore.HookItemLoader.bridge.BaseXPExecutor;
+import cc.hicore.HookItemLoader.bridge.MethodContainer;
+import cc.hicore.HookItemLoader.bridge.UIInfo;
 import cc.hicore.ReflectUtils.MClass;
 import cc.hicore.ReflectUtils.MField;
 import cc.hicore.ReflectUtils.MMethod;
 import cc.hicore.ReflectUtils.XPBridge;
-import cc.hicore.UIItem;
 import cc.hicore.qtool.HookEnv;
 import cc.hicore.qtool.QQMessage.QQMsgSender;
 import cc.hicore.qtool.XposedInit.EnvHook;
 import cc.hicore.qtool.XposedInit.ItemLoader.BaseHookItem;
 import cc.hicore.qtool.XposedInit.ItemLoader.BaseUiItem;
 import cc.hicore.qtool.XposedInit.ItemLoader.HookLoader;
-
-@HookItem(isDelayInit = false, isRunInAllProc = false)
-@UIItem(name = "apk上传失败自动压缩为ZIP重试",type = 1,id = "RenameApkToRename",targetID = 1,groupName = "功能辅助")
-public class FileFailureRename extends BaseHookItem implements BaseUiItem {
-    boolean IsEnable;
-
-    @Override
-    public boolean startHook() throws Throwable {
-        Method m = getMethod();
-        XPBridge.HookBefore(m, param -> {
-            if (!IsEnable) return;
+@XPItem(name = "apk上传失败替换",itemType = XPItem.ITEM_Hook)
+public class FileFailureRename{
+    @VerController
+    @UIItem
+    public UIInfo getUI(){
+        UIInfo ui = new UIInfo();
+        ui.name = "apk上传失败替换";
+        ui.desc = "apk在群聊上传失败自动压缩为ZIP重试";
+        ui.groupName = "功能辅助";
+        ui.targetID = 1;
+        ui.type = 1;
+        return ui;
+    }
+    @VerController
+    @MethodScanner
+    public void getHookMethod(MethodContainer container){
+        container.addMethod("hook",MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.filemanager.uftwrapper.QFileTroopTransferWrapper$TroopUploadWrapper"), null, void.class,
+                new Class[]{MClass.loadClass("com.tencent.mobileqq.uftransfer.api.IUFTTransferKey"), int.class,
+                        MClass.loadClass("com.tencent.mobileqq.uftransfer.api.IUFTUploadCompleteInfo")}));
+    }
+    @VerController
+    @XPExecutor(methodID = "hook")
+    public BaseXPExecutor worker(){
+        return param -> {
             int i = (int) param.args[1];
             if (i == 202) {
                 Object item = MField.GetFirstField(param.thisObject, MClass.loadClass("com.tencent.mobileqq.troop.utils.TroopFileTransferManager$Item"));
@@ -67,40 +87,8 @@ public class FileFailureRename extends BaseHookItem implements BaseUiItem {
 
                 }
             }
-        });
-        return true;
+        };
     }
-
-    @Override
-    public boolean isEnable() {
-        return IsEnable;
-    }
-
-    @Override
-    public boolean check() {
-        return getMethod() != null;
-    }
-
-    @Override
-    public void SwitchChange(boolean IsCheck) {
-        IsEnable = IsCheck;
-        if (IsCheck) {
-            HookLoader.CallHookStart(FileFailureRename.class.getName());
-        }
-    }
-
-    @Override
-    public void ListItemClick(Context context) {
-
-    }
-
-    public Method getMethod() {
-        Method m = MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.filemanager.uftwrapper.QFileTroopTransferWrapper$TroopUploadWrapper"), null, void.class,
-                new Class[]{MClass.loadClass("com.tencent.mobileqq.uftransfer.api.IUFTTransferKey"), int.class,
-                        MClass.loadClass("com.tencent.mobileqq.uftransfer.api.IUFTUploadCompleteInfo")});
-        return m;
-    }
-
     public static String GetPackageInfo(String Path) {
         PackageManager manager = HookEnv.AppContext.getPackageManager();
         PackageInfo info = manager.getPackageArchiveInfo(Path, PackageManager.GET_ACTIVITIES);
