dd91e6f1e8e538e71df9a4e530f48549b1797ca8
Hicores <hicores@163.com>
2022-06-27 22:27:33 +0800

    replace HideEmoPanelGifAndSelf

--- QTool/src/main/java/cc/hicore/qtool/QQCleaner/QQCleanerHook/HideEmoPanelGifAndSelf.java
+++ QTool/src/main/java/cc/hicore/qtool/QQCleaner/QQCleanerHook/HideEmoPanelGifAndSelf.java
@@ -11,21 +11,36 @@ import java.util.Iterator;
 import java.util.List;
 
 import cc.hicore.HookItem;
+import cc.hicore.HookItemLoader.Annotations.MethodScanner;
+import cc.hicore.HookItemLoader.Annotations.UIClick;
+import cc.hicore.HookItemLoader.Annotations.UIItem;
+import cc.hicore.HookItemLoader.Annotations.VerController;
+import cc.hicore.HookItemLoader.Annotations.XPExecutor;
+import cc.hicore.HookItemLoader.Annotations.XPItem;
+import cc.hicore.HookItemLoader.bridge.BaseXPExecutor;
+import cc.hicore.HookItemLoader.bridge.MethodContainer;
+import cc.hicore.HookItemLoader.bridge.UIInfo;
 import cc.hicore.ReflectUtils.MField;
 import cc.hicore.ReflectUtils.MMethod;
 import cc.hicore.ReflectUtils.XPBridge;
-import cc.hicore.UIItem;
 import cc.hicore.Utils.Utils;
 import cc.hicore.qtool.HookEnv;
 import cc.hicore.qtool.XposedInit.ItemLoader.BaseHookItem;
 import cc.hicore.qtool.XposedInit.ItemLoader.BaseUiItem;
+@XPItem(name = "隐藏表情面板图标",itemType = XPItem.ITEM_Hook)
+public class HideEmoPanelGifAndSelf{
+    @VerController
+    @UIItem
+    public UIInfo getUI(){
+        UIInfo ui = new UIInfo();
+        ui.name = "隐藏表情面板图标";
+        ui.type = 1;
+        ui.targetID = 2;
+        ui.groupName = "聊天界面净化";
+        return ui;
+    }
 
-@UIItem(name = "隐藏表情面板图标", type = 2, id = "HideGIFAndSelfPicPanel", targetID = 2,groupName = "聊天界面净化")
-@HookItem(isRunInAllProc = false, isDelayInit = true)
-public class HideEmoPanelGifAndSelf extends BaseHookItem implements BaseUiItem {
-    static HashMap<String, String> HideMap = new HashMap<>();
-
-    {
+    static HashMap<String, String> HideMap = new HashMap<>();{
         HideMap.put("加号", "13");
         HideMap.put("自带表情", "7");
         HideMap.put("收藏表情", "4");
@@ -35,12 +50,16 @@ public class HideEmoPanelGifAndSelf extends BaseHookItem implements BaseUiItem {
         HideMap.put("DIY表情", "11");
         HideMap.put("魔法表情", "9");
     }
-
-    @Override
-    public boolean startHook() throws Throwable {
-        XPBridge.HookAfter(getMethod(), param -> {
+    @VerController
+    @MethodScanner
+    public void getHookMethod(MethodContainer container){
+        container.addMethod("hook",MMethod.FindMethod("com.tencent.mobileqq.emoticonview.EmoticonPanelController", "getPanelDataList", List.class, new Class[0]));
+    }
+    @VerController
+    @XPExecutor(methodID = "hook",period = XPExecutor.After)
+    public BaseXPExecutor hookWorker(){
+        return param -> {
             List<String> mSetConf = HookEnv.Config.getList("QQCleaner", "HideEmoPanel", true);
-
             List l = (List) param.getResult();
             Iterator it = l.iterator();
             while (it.hasNext()) {
@@ -49,33 +68,11 @@ public class HideEmoPanelGifAndSelf extends BaseHookItem implements BaseUiItem {
                 String typeStr = Integer.toString(type);
                 if (mSetConf.contains(typeStr)) it.remove();
             }
-
-        });
-        return true;
+        };
     }
-
-    private Method getMethod() {
-        Method m = MMethod.FindMethod("com.tencent.mobileqq.emoticonview.EmoticonPanelController", "getPanelDataList", List.class, new Class[0]);
-        return m;
-    }
-
-    @Override
-    public boolean isEnable() {
-        return true;
-    }
-
-    @Override
-    public boolean check() {
-        return getMethod() != null;
-    }
-
-    @Override
-    public void SwitchChange(boolean IsCheck) {
-
-    }
-
-    @Override
-    public void ListItemClick(Context context) {
+    @VerController
+    @UIClick
+    public void uiClick(Context context){
         Activity act = Utils.getTopActivity();
         List<String> mSetConf = HookEnv.Config.getList("QQCleaner", "HideEmoPanel", true);
         ArrayList<String> showStrs = new ArrayList<>();
@@ -92,14 +89,14 @@ public class HideEmoPanelGifAndSelf extends BaseHookItem implements BaseUiItem {
                 .setMultiChoiceItems(checkStr, checkStatus, (dialog, which, isChecked) -> {
 
                 }).setNeutralButton("保存", (dialog, which) -> {
-            ArrayList<String> setSave = new ArrayList<>();
-            for (int i = 0; i < checkStatus.length; i++) {
-                if (checkStatus[i]) {
-                    String strIntType = HideMap.get(showStrs.get(i));
-                    setSave.add(strIntType);
-                }
-            }
-            HookEnv.Config.setList("QQCleaner", "HideEmoPanel", setSave);
-        }).show();
+                    ArrayList<String> setSave = new ArrayList<>();
+                    for (int i = 0; i < checkStatus.length; i++) {
+                        if (checkStatus[i]) {
+                            String strIntType = HideMap.get(showStrs.get(i));
+                            setSave.add(strIntType);
+                        }
+                    }
+                    HookEnv.Config.setList("QQCleaner", "HideEmoPanel", setSave);
+                }).show();
     }
 }
