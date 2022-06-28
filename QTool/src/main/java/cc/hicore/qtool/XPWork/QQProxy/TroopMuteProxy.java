743e69713fa92b37c6a9c5a7281c4e1ff11add8b
Hicores <hicores@163.com>
2022-06-28 12:10:18 +0800

    replace TroopMuteProxy

--- QTool/src/main/java/cc/hicore/qtool/XPWork/QQProxy/TroopMuteProxy.java
+++ QTool/src/main/java/cc/hicore/qtool/XPWork/QQProxy/TroopMuteProxy.java
@@ -3,34 +3,59 @@ package cc.hicore.qtool.XPWork.QQProxy;
 import java.lang.reflect.Method;
 
 import cc.hicore.HookItem;
+import cc.hicore.HookItemLoader.Annotations.MethodScanner;
+import cc.hicore.HookItemLoader.Annotations.VerController;
+import cc.hicore.HookItemLoader.Annotations.XPExecutor;
+import cc.hicore.HookItemLoader.Annotations.XPItem;
+import cc.hicore.HookItemLoader.bridge.BaseXPExecutor;
+import cc.hicore.HookItemLoader.bridge.MethodContainer;
 import cc.hicore.ReflectUtils.MClass;
 import cc.hicore.ReflectUtils.MMethod;
 import cc.hicore.ReflectUtils.XPBridge;
 import cc.hicore.qtool.JavaPlugin.Controller.PluginMessageProcessor;
 import cc.hicore.qtool.QQManager.QQEnvUtils;
 import cc.hicore.qtool.XposedInit.ItemLoader.BaseHookItem;
-
-@HookItem(isDelayInit = false, isRunInAllProc = false)
-public class TroopMuteProxy extends BaseHookItem {
+@XPItem(name = "Proxy_Troop_Mute",itemType = XPItem.ITEM_Hook)
+public class TroopMuteProxy{
     private static final String TAG = "TroopMuteProxy";
-
-    @Override
-    public String getTag() {
-        return TAG;
+    @VerController
+    @MethodScanner
+    public void getHookMethod(MethodContainer container){
+        container.addMethod("hook_1",MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.troop.utils.TroopGagMgr"), null, void.class, new Class[]{
+                String.class,
+                long.class,
+                long.class,
+                int.class,
+                String.class,
+                String.class,
+                boolean.class
+        }));
+        container.addMethod("hook_2",MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.troop.utils.TroopGagMgr"), null, void.class, new Class[]{
+                String.class,
+                String.class,
+                long.class,
+                long.class,
+                int.class,
+                boolean.class,
+                boolean.class
+        }));
     }
-
-    @Override
-    public boolean startHook() {
-        Method[] m = getMethod();
-        XPBridge.HookBefore(m[0], param -> {
+    @VerController
+    @XPExecutor(methodID = "hook_1")
+    public BaseXPExecutor worker_1(){
+        return param -> {
             String GroupUin = (String) param.args[0];
             long TimeRest = (long) param.args[2];
             String AdminUin = (String) param.args[4];
             String Target = (String) param.args[5];
 
             PluginMessageProcessor.submit(() -> PluginMessageProcessor.onMuteEvent(GroupUin, Target, AdminUin, TimeRest));
-        });
-        XPBridge.HookBefore(m[1], param -> {
+        };
+    }
+    @VerController
+    @XPExecutor(methodID = "hook_2")
+    public BaseXPExecutor worker_2(){
+        return param -> {
             String GroupUin = (String) param.args[0];
             String AdminUin = (String) param.args[1];
             long TimeRest = (long) param.args[3];
@@ -41,42 +66,6 @@ public class TroopMuteProxy extends BaseHookItem {
             } else {
                 PluginMessageProcessor.submit(() -> PluginMessageProcessor.onMuteEvent(GroupUin, QQEnvUtils.getCurrentUin(), AdminUin, TimeRest));
             }
-
-        });
-        return true;
-    }
-
-    @Override
-    public boolean isEnable() {
-        return true;
-    }
-
-    @Override
-    public boolean check() {
-        Method[] m = getMethod();
-        return m[0] != null && m[1] != null;
-    }
-
-    public Method[] getMethod() {
-        Method[] m = new Method[2];
-        m[0] = MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.troop.utils.TroopGagMgr"), null, void.class, new Class[]{
-                String.class,
-                long.class,
-                long.class,
-                int.class,
-                String.class,
-                String.class,
-                boolean.class
-        });
-        m[1] = MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.troop.utils.TroopGagMgr"), null, void.class, new Class[]{
-                String.class,
-                String.class,
-                long.class,
-                long.class,
-                int.class,
-                boolean.class,
-                boolean.class
-        });
-        return m;
+        };
     }
 }
