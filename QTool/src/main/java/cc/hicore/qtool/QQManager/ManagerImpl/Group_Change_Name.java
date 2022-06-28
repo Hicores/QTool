57ad183c49bd154581f38ff799d64a62626028c0
Hicores <hicores@163.com>
2022-06-28 12:58:46 +0800

    replace Group_Change_Name

---
+++ QTool/src/main/java/cc/hicore/qtool/QQManager/ManagerImpl/Group_Change_Name.java
@@ -0,0 +1,51 @@
+package cc.hicore.qtool.QQManager.ManagerImpl;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+import cc.hicore.HookItemLoader.Annotations.ApiExecutor;
+import cc.hicore.HookItemLoader.Annotations.VerController;
+import cc.hicore.HookItemLoader.Annotations.XPChecker;
+import cc.hicore.HookItemLoader.Annotations.XPItem;
+import cc.hicore.ReflectUtils.MClass;
+import cc.hicore.ReflectUtils.MField;
+import cc.hicore.ReflectUtils.MMethod;
+import cc.hicore.Utils.Assert;
+import cc.hicore.qtool.QQManager.QQEnvUtils;
+
+@XPItem(name = "Group_Change_Name",itemType = XPItem.ITEM_Api)
+public class Group_Change_Name {
+    @VerController
+    @ApiExecutor
+    public void change(String GroupUin, String UserUin, String name) throws Exception {
+        Object mCallObj = MClass.NewInstance(MClass.loadClass("com.tencent.mobileqq.troop.handler.TroopMemberCardHandler"), new Class[]{MClass.loadClass("com.tencent.common.app.AppInterface")}, QQEnvUtils.getAppRuntime());
+        Object TroopCardObj = MClass.NewInstance(MClass.loadClass("com.tencent.mobileqq.data.troop.TroopMemberCardInfo"), new Class[0], new Object[0]);
+        MField.SetField(TroopCardObj, "name", name);
+        MField.SetField(TroopCardObj, "troopuin", GroupUin);
+
+        MField.SetField(TroopCardObj, "memberuin", UserUin);
+        MField.SetField(TroopCardObj, "email", "");
+        MField.SetField(TroopCardObj, "memo", "");
+        MField.SetField(TroopCardObj, "tel", "");
+        ArrayList mList = new ArrayList();
+        ArrayList mList2 = new ArrayList();
+        mList.add(TroopCardObj);
+        mList2.add(1);
+        Method CallMethod = MMethod.FindMethod("com.tencent.mobileqq.troop.handler.TroopMemberCardHandler", null, void.class, new Class[]{
+                String.class,
+                ArrayList.class,
+                ArrayList.class
+        });
+        CallMethod.invoke(mCallObj, GroupUin, mList, mList2);
+    }
+    @VerController
+    @XPChecker
+    public void check(){
+        Object callMethod = MMethod.FindMethod("com.tencent.mobileqq.troop.handler.TroopMemberCardHandler", null, void.class, new Class[]{
+                String.class,
+                ArrayList.class,
+                ArrayList.class
+        });
+        Assert.notNull(callMethod,"callMethod is NULL");
+    }
+}
--- QTool/src/main/java/cc/hicore/qtool/QQManager/QQGroupManager.java
+++ QTool/src/main/java/cc/hicore/qtool/QQManager/QQGroupManager.java
@@ -7,116 +7,32 @@ import java.lang.reflect.Proxy;
 import java.util.ArrayList;
 import java.util.List;
 
+import cc.hicore.HookItemLoader.core.ApiHelper;
 import cc.hicore.LogUtils.LogUtils;
 import cc.hicore.ReflectUtils.MClass;
 import cc.hicore.ReflectUtils.MField;
 import cc.hicore.ReflectUtils.MMethod;
 import cc.hicore.qtool.HookEnv;
+import cc.hicore.qtool.QQManager.ManagerImpl.Group_Change_Title;
+import cc.hicore.qtool.QQManager.ManagerImpl.Group_Kick;
+import cc.hicore.qtool.QQManager.ManagerImpl.Group_Kick_Multi;
+import cc.hicore.qtool.QQManager.ManagerImpl.Group_Mute;
 
 
 public class QQGroupManager {
     public static void Group_Kick(String GroupUin, String UserUin, boolean isBlack) {
-        try {
-            Object ManagerObject = MClass.NewInstance(MClass.loadClass("com.tencent.mobileqq.troop.handler.TroopMemberMngHandler"), QQEnvUtils.getAppRuntime());
-            ArrayList<Long> KickList = new ArrayList<>();
-            KickList.add(Long.parseLong(UserUin));
-            Method CallMethod = MMethod.FindMethod("com.tencent.mobileqq.troop.handler.TroopMemberMngHandler", null, void.class, new Class[]{
-                    long.class,
-                    List.class,
-                    boolean.class,
-                    boolean.class
-            });
-            CallMethod.invoke(ManagerObject,
-                    Long.parseLong(GroupUin), KickList, isBlack, false
-            );
-        } catch (Throwable th) {
-            LogUtils.error("Group_Kick", th);
-        }
+        ApiHelper.invoke(Group_Kick.class,GroupUin,UserUin,isBlack);
     }
     public static void Group_Kick(String GroupUin, String[] UserUin, boolean isBlack) {
-        try {
-            Object ManagerObject = MClass.NewInstance(MClass.loadClass("com.tencent.mobileqq.troop.handler.TroopMemberMngHandler"), QQEnvUtils.getAppRuntime());
-            ArrayList<Long> KickList = new ArrayList<>();
-            for (String Uin : UserUin) KickList.add(Long.parseLong(Uin));
-            Method CallMethod = MMethod.FindMethod("com.tencent.mobileqq.troop.handler.TroopMemberMngHandler", null, void.class, new Class[]{
-                    long.class,
-                    List.class,
-                    boolean.class,
-                    boolean.class
-            });
-            CallMethod.invoke(ManagerObject,
-                    Long.parseLong(GroupUin), KickList, isBlack, false
-            );
-        } catch (Throwable th) {
-            LogUtils.error("Group_Kick_All", th);
-        }
+        ApiHelper.invoke(Group_Kick_Multi.class,GroupUin,UserUin,isBlack);
     }
-
     public static void Group_Mute(String GroupUin, String UserUin, long time) {
-        if (GroupUin.contains("&")) {
-            String[] GuildCut = GroupUin.split("&");
-            QQGuildManager.Guild_Mute(GuildCut[0], UserUin, time);
-            return;
-        }
-        try {
-
-            if (TextUtils.isEmpty(UserUin)) {
-                if (time != 0 && time < 60)time = 268435455;
-                Object TroopGagManager = MMethod.CallMethodSingle(QQEnvUtils.getAppRuntime(), "getBusinessHandler",
-                        MClass.loadClass("com.tencent.mobileqq.app.BusinessHandler"),
-                        MField.GetStaticField(MClass.loadClass("com.tencent.mobileqq.app.BusinessHandlerFactory"), "TROOP_GAG_HANDLER"));
-                MMethod.CallMethodParams(TroopGagManager, null, void.class, GroupUin, time);
-            } else {
-                Object TroopGagManager = MMethod.CallMethodSingle(QQEnvUtils.getAppRuntime(), "getManager",
-                        MClass.loadClass("mqq.manager.Manager"),
-                        MField.GetStaticField(MClass.loadClass("com.tencent.mobileqq.app.QQManagerFactory"), "TROOP_GAG_MANAGER"));
-                MMethod.CallMethodParams(TroopGagManager, null, boolean.class, GroupUin, UserUin, time);
-            }
-        } catch (Exception e) {
-            LogUtils.error("Group_Mute", e);
-        }
+        ApiHelper.invoke(Group_Mute.class,GroupUin,UserUin,time);
     }
-
     public static void Group_Change_Title(String GroupUin, String UserUin, String title) {
-        try {
-            Object mProxy = Proxy.newProxyInstance(HookEnv.mLoader, new Class[]{MClass.loadClass("mqq.observer.BusinessObserver")}, (proxy, method, args) -> null);
-            MMethod.CallMethod(null, MClass.loadClass("com.tencent.biz.troop.EditUniqueTitleActivity"), null, void.class, new Class[]{
-                            HookEnv.AppInterface.getClass(), String.class, String.class, String.class, MClass.loadClass("mqq.observer.BusinessObserver")
-                    },
-                    HookEnv.AppInterface, GroupUin, UserUin, title, mProxy);
-        } catch (Exception e) {
-            LogUtils.error("Group_Change_Title", e);
-        }
-
+        ApiHelper.invoke(Group_Change_Title.class,GroupUin,UserUin,title);
     }
-
     public static void Group_Change_Name(String GroupUin, String UserUin, String name) {
-        try {
-            Object mCallObj = MClass.NewInstance(MClass.loadClass("com.tencent.mobileqq.troop.handler.TroopMemberCardHandler"), new Class[]{MClass.loadClass("com.tencent.common.app.AppInterface")}, QQEnvUtils.getAppRuntime());
-            Object TroopCardObj = MClass.NewInstance(MClass.loadClass("com.tencent.mobileqq.data.troop.TroopMemberCardInfo"), new Class[0], new Object[0]);
-            MField.SetField(TroopCardObj, "name", name);
-            MField.SetField(TroopCardObj, "troopuin", GroupUin);
-
-            MField.SetField(TroopCardObj, "memberuin", UserUin);
-            MField.SetField(TroopCardObj, "email", "");
-            MField.SetField(TroopCardObj, "memo", "");
-            MField.SetField(TroopCardObj, "tel", "");
-            ArrayList mList = new ArrayList();
-            ArrayList mList2 = new ArrayList();
-            mList.add(TroopCardObj);
-            mList2.add(1);
-            Method CallMethod = MMethod.FindMethod("com.tencent.mobileqq.troop.handler.TroopMemberCardHandler", null, void.class, new Class[]{
-                    String.class,
-                    ArrayList.class,
-                    ArrayList.class
-            });
-            CallMethod.invoke(mCallObj, GroupUin, mList, mList2);
-        } catch (Exception e) {
-            LogUtils.error("Group_Change_Name", e);
-        }
-    }
-
-    public static void Group_Exit(String GroupUin) {
-        //Nothing Here
+        ApiHelper.invoke(Group_Change_Title.class,GroupUin,UserUin,name);
     }
 }
