62f4bbc085b56d5d39d6e81a33b3fe7326f03cff
Hicores <hicores@163.com>
2022-06-28 12:18:32 +0800

    replace AppCenterTracker

--- QTool/src/main/java/cc/hicore/Tracker/AppCenterTracker.java
+++ QTool/src/main/java/cc/hicore/Tracker/AppCenterTracker.java
@@ -5,12 +5,16 @@ import com.microsoft.appcenter.analytics.Analytics;
 import java.util.HashMap;
 
 import cc.hicore.HookItem;
+import cc.hicore.HookItemLoader.Annotations.CommonExecutor;
+import cc.hicore.HookItemLoader.Annotations.VerController;
+import cc.hicore.HookItemLoader.Annotations.XPExecutor;
+import cc.hicore.HookItemLoader.Annotations.XPItem;
 import cc.hicore.qtool.BuildConfig;
 import cc.hicore.qtool.QQManager.QQEnvUtils;
 import cc.hicore.qtool.XposedInit.ItemLoader.BaseHookItem;
 
-@HookItem(isDelayInit = true, isRunInAllProc = false)
-public class AppCenterTracker extends BaseHookItem {
+@XPItem(name = "AppCenter_Init",itemType = XPItem.ITEM_Hook,period = XPItem.Period_InitData)
+public class AppCenterTracker{
     public static void StartTrack() {
         try {
             Thread.sleep(5000);
@@ -22,20 +26,10 @@ public class AppCenterTracker extends BaseHookItem {
         trackData.put("UseVer", BuildConfig.VERSION_NAME);
         Analytics.trackEvent("QTLoadInMainProc", trackData);
     }
-
-    @Override
-    public boolean startHook() throws Throwable {
+    @VerController
+    @CommonExecutor
+    public void startHook(){
         new Thread(AppCenterTracker::StartTrack).start();
-        return true;
     }
 
-    @Override
-    public boolean isEnable() {
-        return true;
-    }
-
-    @Override
-    public boolean check() {
-        return true;
-    }
 }
