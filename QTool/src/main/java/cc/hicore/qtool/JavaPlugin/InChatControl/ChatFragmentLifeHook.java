5b028b46a43c8f6bf1fa2a3d9b5853acd24dab51
Hicores <hicores@163.com>
2022-06-28 12:34:52 +0800

    replace ChatFragmentLifeHook

--- QTool/src/main/java/cc/hicore/qtool/JavaPlugin/InChatControl/ChatFragmentLifeHook.java
+++ QTool/src/main/java/cc/hicore/qtool/JavaPlugin/InChatControl/ChatFragmentLifeHook.java
@@ -6,6 +6,14 @@ import android.os.Looper;
 import java.lang.reflect.Method;
 
 import cc.hicore.HookItem;
+import cc.hicore.HookItemLoader.Annotations.MethodScanner;
+import cc.hicore.HookItemLoader.Annotations.VerController;
+import cc.hicore.HookItemLoader.Annotations.XPExecutor;
+import cc.hicore.HookItemLoader.Annotations.XPItem;
+import cc.hicore.HookItemLoader.bridge.BaseXPExecutor;
+import cc.hicore.HookItemLoader.bridge.MethodContainer;
+import cc.hicore.HookItemLoader.bridge.MethodFinderBuilder;
+import cc.hicore.HookItemLoader.bridge.QQVersion;
 import cc.hicore.ReflectUtils.MClass;
 import cc.hicore.ReflectUtils.MField;
 import cc.hicore.ReflectUtils.MMethod;
@@ -17,32 +25,30 @@ import cc.hicore.qtool.XposedInit.MethodFinder;
 /*
 挂钩判断聊天窗口显示/隐藏事件
  */
-@HookItem(isDelayInit = false, isRunInAllProc = false)
-public class ChatFragmentLifeHook extends BaseHookItem {
-    @Override
-    public boolean startHook() throws Throwable {
-
-        Method[] hookMethod = getHookMethod();
-        XPBridge.HookAfter(hookMethod[0], param -> {
-            onShow(param.thisObject);
-        });
-        XPBridge.HookAfter(hookMethod[1], param -> {
-            onHide(param.thisObject);
-        });
-        return true;
+@XPItem(name = "ChatFragmentLifeHook",itemType = XPItem.ITEM_Hook)
+public class ChatFragmentLifeHook{
+    @VerController(max_targetVer = QQVersion.QQ_8_8_93)
+    @MethodScanner
+    public void getHookMethod(MethodContainer container){
+        container.addMethod("ChatOnShow",MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.activity.aio.core.BaseChatPie"), "U", new Class[0]));
+        container.addMethod("ChatOnHide",MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.activity.aio.core.BaseChatPie"), "R", new Class[0]));
     }
-
-    @Override
-    public boolean isEnable() {
-        return true;
+    @VerController(targetVer = QQVersion.QQ_8_8_93)
+    @MethodScanner
+    public void getHookMethod_8_8_93(MethodContainer container){
+        container.addMethod(MethodFinderBuilder.newFinderByString("ChatOnShow","loadBackgroundAsync: skip for mosaic is on",m->m.getDeclaringClass().getName().equals("com.tencent.mobileqq.activity.aio.core.BaseChatPie")));
+        container.addMethod(MethodFinderBuilder.newFinderByString("ChatOnHide","doOnStop",m->m.getDeclaringClass().getName().equals("com.tencent.mobileqq.activity.aio.core.BaseChatPie")));
     }
-
-    @Override
-    public boolean check() {
-        Method[] m = getHookMethod();
-        return m[0] != null && m[1] != null;
+    @VerController
+    @XPExecutor(methodID = "ChatOnShow",period = XPExecutor.After)
+    public BaseXPExecutor onShow(){
+        return param -> onShow(param.thisObject);
     }
-
+    @VerController
+    @XPExecutor(methodID = "ChatOnHide",period = XPExecutor.After)
+    public BaseXPExecutor onHide(){
+        return param -> onHide(param.thisObject);
+    }
     private void onShow(Object pie) {
         new Handler(Looper.getMainLooper())
                 .postDelayed(() -> {
@@ -66,25 +72,5 @@ public class ChatFragmentLifeHook extends BaseHookItem {
                         e.printStackTrace();
                     }
                 }, 500);
-    }
-
-    public Method[] getHookMethod() {
-        Method[] m = new Method[2];
-        if (HostInfo.getVerCode() > 8000){
-            m[0] = MethodFinder.findMethodFromCache("ChatOnShow");
-            if (m[0] == null){
-                MethodFinder.NeedReportToFindMethod("ChatOnShow","SH","loadBackgroundAsync: skip for mosaic is on",a->a.getDeclaringClass().getName().equals("com.tencent.mobileqq.activity.aio.core.BaseChatPie"));
-            }
-
-            m[1] = MethodFinder.findMethodFromCache("ChatOnHide");
-            if (m[1] == null){
-                MethodFinder.NeedReportToFindMethod("ChatOnHide","SH","doOnStop",a->a.getDeclaringClass().getName().equals("com.tencent.mobileqq.activity.aio.core.BaseChatPie"));
-            }
-        }else {
-            m[0] = MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.activity.aio.core.BaseChatPie"), "U", new Class[0]);
-            m[1] = MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.activity.aio.core.BaseChatPie"), "R", new Class[0]);
-        }
-
-        return m;
     }
 }
