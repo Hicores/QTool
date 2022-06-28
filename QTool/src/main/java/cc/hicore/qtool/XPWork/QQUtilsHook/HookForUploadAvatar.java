3436801c6dc8dc2ebec959b228bafac5eb4dfd0d
Hicores <hicores@163.com>
2022-06-28 12:15:15 +0800

    replace HookForUploadAvatar

--- QTool/src/main/java/cc/hicore/qtool/XPWork/QQUtilsHook/HookForUploadAvatar.java
+++ QTool/src/main/java/cc/hicore/qtool/XPWork/QQUtilsHook/HookForUploadAvatar.java
@@ -8,88 +8,74 @@ import java.io.OutputStream;
 import java.lang.reflect.Method;
 
 import cc.hicore.HookItem;
+import cc.hicore.HookItemLoader.Annotations.MethodScanner;
+import cc.hicore.HookItemLoader.Annotations.UIItem;
+import cc.hicore.HookItemLoader.Annotations.VerController;
+import cc.hicore.HookItemLoader.Annotations.XPExecutor;
+import cc.hicore.HookItemLoader.Annotations.XPItem;
+import cc.hicore.HookItemLoader.bridge.BaseXPExecutor;
+import cc.hicore.HookItemLoader.bridge.MethodContainer;
+import cc.hicore.HookItemLoader.bridge.UIInfo;
 import cc.hicore.LogUtils.LogUtils;
 import cc.hicore.ReflectUtils.MClass;
 import cc.hicore.ReflectUtils.MMethod;
 import cc.hicore.ReflectUtils.XPBridge;
-import cc.hicore.UIItem;
 import cc.hicore.Utils.DebugUtils;
 import cc.hicore.qtool.XposedInit.ItemLoader.BaseHookItem;
 import cc.hicore.qtool.XposedInit.ItemLoader.BaseUiItem;
 import cc.hicore.qtool.XposedInit.ItemLoader.HookLoader;
-
-@HookItem(isDelayInit = false, isRunInAllProc = false)
-@UIItem(name = "半透明头像上传", type = 1, id = "Upload_Avatar", targetID = 1,groupName = "功能辅助")
-public class HookForUploadAvatar extends BaseHookItem implements BaseUiItem {
-    boolean IsEnable = false;
-
-    @Override
-    public boolean startHook() throws Throwable {
-        Method qqMethod = getMethod();
-        XPBridge.HookBefore(qqMethod, param -> {
-            try {
-                if (!IsEnable) return;
-                //自己进行图像转换,不给QQ把透明背景扣掉的机会
-                FileOutputStream fos = new FileOutputStream((String) param.args[0]);
-                Bitmap bitmap = (Bitmap) param.args[1];
-                bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
-                fos.close();
-                param.setResult(true);
-            } catch (Throwable e) {
-                LogUtils.error("HookForUploadAvatar", e);
-            }
-        });
-
-        Method systemMethod = Bitmap.class.getMethod("compress", Bitmap.CompressFormat.class, int.class, OutputStream.class);
-        XPBridge.HookBefore(systemMethod, param -> {
-            if (!IsEnable) return;
-            String CurrentCallStacks = DebugUtils.getCurrentCallStacks();
-            if (CurrentCallStacks.contains("NearbyPeoplePhotoUploadProcessor") || CurrentCallStacks.contains("doInBackground") ||
-                    CurrentCallStacks.contains("TroopUploadingThread")) {
-                param.args[0] = Bitmap.CompressFormat.PNG;
-            }
-        });
-
-        Method m = MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.troop.avatar.UploadItem"),"a",new Class[]{
-                int.class
-        });
-        XPBridge.HookAfter(m,param -> {
-            param.setResult(String.valueOf(param.getResult()).replace("imagetype=5","imagetype=2").replace("filetype=3","filetype=2"));
-        });
-        return true;
+@XPItem(name = "半透明头像上传",itemType = XPItem.ITEM_Hook)
+public class HookForUploadAvatar{
+    @VerController
+    @UIItem
+    public UIInfo getUI(){
+        UIInfo ui = new UIInfo();
+        ui.name = "半透明头像上传";
+        ui.groupName = "功能辅助";
+        ui.type = 1;
+        ui.targetID = 1;
+        return ui;
     }
-
-    @Override
-    public boolean isEnable() {
-        return IsEnable;
-    }
-
-    @Override
-    public boolean check() {
-        return getMethod() != null;
-    }
-
-    @Override
-    public void SwitchChange(boolean IsCheck) {
-        IsEnable = IsCheck;
-        if (IsCheck) {
-            HookLoader.CallHookStart(HookForUploadAvatar.class.getName());
-        }
-    }
-
-    private Method getMethod() {
-        Method hookMethod = MMethod.FindMethod("com.tencent.mobileqq.pic.compress.Utils", null, boolean.class, new Class[]{
+    @VerController
+    @MethodScanner
+    public void getHookMethod(MethodContainer container) throws NoSuchMethodException {
+        container.addMethod("hook",MMethod.FindMethod("com.tencent.mobileqq.pic.compress.Utils", null, boolean.class, new Class[]{
                 String.class,
                 Bitmap.class,
                 int.class,
                 String.class,
                 MClass.loadClass("com.tencent.mobileqq.pic.CompressInfo")
-        });
-        return hookMethod;
+        }));
+        container.addMethod("hook_2",Bitmap.class.getMethod("compress", Bitmap.CompressFormat.class, int.class, OutputStream.class));
+        container.addMethod("hook_3",MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.troop.avatar.UploadItem"),"a",new Class[]{
+                int.class
+        }));
     }
-
-    @Override
-    public void ListItemClick(Context context) {
-
+    @VerController
+    @XPExecutor(methodID = "hook")
+    public BaseXPExecutor worker_1(){
+        return param -> {
+            FileOutputStream fos = new FileOutputStream((String) param.args[0]);
+            Bitmap bitmap = (Bitmap) param.args[1];
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.close();
+            param.setResult(true);
+        };
+    }
+    @VerController
+    @XPExecutor(methodID = "hook_2")
+    public BaseXPExecutor worker_2(){
+        return param -> {
+            String CurrentCallStacks = DebugUtils.getCurrentCallStacks();
+            if (CurrentCallStacks.contains("NearbyPeoplePhotoUploadProcessor") || CurrentCallStacks.contains("doInBackground") ||
+                    CurrentCallStacks.contains("TroopUploadingThread")) {
+                param.args[0] = Bitmap.CompressFormat.PNG;
+            }
+        };
+    }
+    @VerController
+    @XPExecutor(methodID = "hook_3",period = XPExecutor.After)
+    public BaseXPExecutor worker_3(){
+        return param -> param.setResult(String.valueOf(param.getResult()).replace("imagetype=5","imagetype=2").replace("filetype=3","filetype=2"));
     }
 }
