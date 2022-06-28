c7e61a1b2528df45ce305fa4a8221246c5a4f1f3
Hicores <hicores@163.com>
2022-06-28 12:07:20 +0800

    replace TroopJoinExitProxy

--- QTool/src/main/java/cc/hicore/qtool/XPWork/QQProxy/TroopJoinExitProxy.java
+++ QTool/src/main/java/cc/hicore/qtool/XPWork/QQProxy/TroopJoinExitProxy.java
@@ -4,27 +4,34 @@ import java.lang.reflect.Method;
 import java.util.HashSet;
 
 import cc.hicore.HookItem;
+import cc.hicore.HookItemLoader.Annotations.MethodScanner;
+import cc.hicore.HookItemLoader.Annotations.VerController;
+import cc.hicore.HookItemLoader.Annotations.XPExecutor;
+import cc.hicore.HookItemLoader.Annotations.XPItem;
+import cc.hicore.HookItemLoader.bridge.BaseXPExecutor;
+import cc.hicore.HookItemLoader.bridge.MethodContainer;
 import cc.hicore.ReflectUtils.MClass;
 import cc.hicore.ReflectUtils.MMethod;
 import cc.hicore.ReflectUtils.XPBridge;
 import cc.hicore.qtool.JavaPlugin.Controller.PluginMessageProcessor;
 import cc.hicore.qtool.XposedInit.ItemLoader.BaseHookItem;
-
-@HookItem(isDelayInit = false, isRunInAllProc = false)
-public class TroopJoinExitProxy extends BaseHookItem {
+@XPItem(name = "Proxy_Troop_Join_Exit",itemType = XPItem.ITEM_Hook)
+public class TroopJoinExitProxy{
     private static final String TAG = "TroopEventProxy";
     private static final HashSet<String> cacheExitMap = new HashSet<>();
     private static final HashSet<String> cacheJoinMap = new HashSet<>();
-
-    @Override
-    public String getTag() {
-        return TAG;
+    @VerController
+    @MethodScanner
+    public void getHookMethod(MethodContainer container){
+        container.addMethod("hook_1",MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.troop.api.impl.TroopCreateInfoServiceImpl"),
+                "deleteInInviteList", new Class[]{String.class, String.class}));
+        container.addMethod("hook_2",MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.troop.api.impl.TroopCreateInfoServiceImpl"),
+                "isInInviteList", new Class[]{String.class, String.class}));
     }
-
-    @Override
-    public boolean startHook() {
-        Method[] m = getMethod();
-        XPBridge.HookBefore(m[0], param -> {
+    @VerController
+    @XPExecutor(methodID = "hook_1")
+    public BaseXPExecutor worker_1(){
+        return param -> {
             String GroupUin = (String) param.args[0];
             String UserUin = (String) param.args[1];
             if (cacheExitMap.contains(GroupUin + "-" + UserUin)) return;
@@ -32,8 +39,12 @@ public class TroopJoinExitProxy extends BaseHookItem {
             cacheExitMap.add(GroupUin + "-" + UserUin);
 
             PluginMessageProcessor.onExitEvent(GroupUin, UserUin, "-1");
-        });
-        XPBridge.HookBefore(m[1], param -> {
+        };
+    }
+    @VerController
+    @XPExecutor(methodID = "hook_2")
+    public BaseXPExecutor worker_2(){
+        return param -> {
             String GroupUin = String.valueOf(param.args[0]);
             String UserUin = (String) param.args[1];
             if (cacheJoinMap.contains(GroupUin + "-" + UserUin)) return;
@@ -41,27 +52,6 @@ public class TroopJoinExitProxy extends BaseHookItem {
             cacheJoinMap.add(GroupUin + "-" + UserUin);
 
             PluginMessageProcessor.onJoinEvent(GroupUin, UserUin, "-1", "-1");
-        });
-        return true;
-    }
-
-    @Override
-    public boolean isEnable() {
-        return true;
-    }
-
-    @Override
-    public boolean check() {
-        Method[] m = getMethod();
-        return m[0] != null && m[1] != null;
-    }
-
-    public Method[] getMethod() {
-        Method[] m = new Method[2];
-        m[0] = MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.troop.api.impl.TroopCreateInfoServiceImpl"),
-                "deleteInInviteList", new Class[]{String.class, String.class});
-        m[1] = MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.troop.api.impl.TroopCreateInfoServiceImpl"),
-                "isInInviteList", new Class[]{String.class, String.class});
-        return m;
+        };
     }
 }
