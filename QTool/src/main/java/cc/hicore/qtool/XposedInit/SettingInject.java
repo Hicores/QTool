ba1e6d2c0a99c021e81967a2a0cc7b86de53c5ce
Hicores <hicores@163.com>
2022-07-03 22:43:43 +0800

    fix settingsInject

--- QTool/src/main/java/cc/hicore/qtool/XposedInit/SettingInject.java
+++ QTool/src/main/java/cc/hicore/qtool/XposedInit/SettingInject.java
@@ -2,16 +2,20 @@ package cc.hicore.qtool.XposedInit;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
+import cc.hicore.ConfigUtils.GlobalConfig;
 import cc.hicore.ReflectUtils.MClass;
 import cc.hicore.ReflectUtils.MField;
 import cc.hicore.ReflectUtils.MMethod;
 import cc.hicore.ReflectUtils.ResUtils;
 import cc.hicore.ReflectUtils.XPBridge;
+import cc.hicore.Utils.Utils;
 import cc.hicore.qtool.ActProxy.MainMenu;
 import cc.hicore.qtool.BuildConfig;
 import cc.hicore.qtool.XPWork.QQUIUtils.FormItemUtils;
@@ -24,9 +28,21 @@ public class SettingInject {
             try {
                 Activity act = (Activity) param.thisObject;
                 ResUtils.StartInject(act);
+                ViewGroup mRoot = null;
+                Class<?> clz = MClass.loadClass("com.tencent.mobileqq.widget.FormSimpleItem");
+                for (Field f : act.getClass().getDeclaredFields()){
+                    try{
+                        if (clz.isAssignableFrom(f.getType())){
+                            f.setAccessible(true);
+                            View item = (View) f.get(act);
+                            mRoot = (ViewGroup) item.getParent();
+                            if (mRoot != null){
+                                break;
+                            }
+                        }
 
-                View item = MField.GetFirstField(act, MClass.loadClass("com.tencent.mobileqq.widget.FormSimpleItem"));
-                ViewGroup mRoot = (ViewGroup) item.getParent();
+                    }catch (Exception ignored){ }
+                }
                 View newItem = FormItemUtils.createMultiItem(act, "QTool", BuildConfig.VERSION_NAME, v -> MainMenu.onCreate(act));
                 newItem.setOnLongClickListener(v -> {
                     DebugDialog.startShow(v.getContext());
@@ -35,6 +51,10 @@ public class SettingInject {
                 mRoot.addView(newItem, 1);
             } catch (Exception e) {
                 XposedBridge.log(e);
+                if (!GlobalConfig.Get_Boolean("Add_Menu_Button_to_Main",false)){
+                    Utils.ShowToastL("QTool无法创建设置选项,已自动开启主界面加号入口\n"+ Log.getStackTraceString(e));
+                    GlobalConfig.Put_Boolean("Add_Menu_Button_to_Main",true);
+                }
             }
         });
     }
