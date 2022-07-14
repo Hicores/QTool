971928f010d374f360fc1c476aefa7516de4b87f
Hicores <hicores@163.com>
2022-07-14 14:11:46 +0800

    适配 去除回复自动艾特

--- QTool/src/main/java/cc/hicore/qtool/ChatHook/ChatCracker/DisableReplayAt.java
+++ QTool/src/main/java/cc/hicore/qtool/ChatHook/ChatCracker/DisableReplayAt.java
@@ -8,6 +8,8 @@ import cc.hicore.HookItemLoader.Annotations.XPExecutor;
 import cc.hicore.HookItemLoader.Annotations.XPItem;
 import cc.hicore.HookItemLoader.bridge.BaseXPExecutor;
 import cc.hicore.HookItemLoader.bridge.MethodContainer;
+import cc.hicore.HookItemLoader.bridge.MethodFinderBuilder;
+import cc.hicore.HookItemLoader.bridge.QQVersion;
 import cc.hicore.HookItemLoader.bridge.UIInfo;
 import cc.hicore.ReflectUtils.MClass;
 import cc.hicore.ReflectUtils.MMethod;
@@ -26,12 +28,22 @@ public class DisableReplayAt{
     }
 
     @MethodScanner
-    @VerController
+    @VerController(max_targetVer = QQVersion.QQ_8_9_0)
     public void findHookMethod(MethodContainer container){
         container.addMethod("hook",MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.activity.aio.rebuild.input.InputUIUtils"),"a",void.class,new Class[]{
                 MClass.loadClass("com.tencent.mobileqq.activity.aio.core.AIOContext"),
                 MClass.loadClass("com.tencent.mobileqq.activity.aio.BaseSessionInfo"),
                 boolean.class
+        }));
+    }
+    @MethodScanner
+    @VerController(targetVer = QQVersion.QQ_8_9_0)
+    public void findHookMethod_890(MethodContainer container){
+        container.addMethod(MethodFinderBuilder.newFinderByString("hook","@",m-> {
+            if (m.getDeclaringClass().getName().startsWith("com.tencent.mobileqq.activity.aio.rebuild.input")){
+                return MMethod.FindMethodByName(m.getDeclaringClass(),"a");
+            }
+            return false;
         }));
     }
 
