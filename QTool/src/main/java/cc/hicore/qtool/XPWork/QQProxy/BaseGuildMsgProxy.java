221853e876507d7666a7557d9ca674e4e2bde8ba
Hicores <hicores@163.com>
2022-07-14 13:16:18 +0800

    适配 Proxy_Guild_Msg

--- QTool/src/main/java/cc/hicore/qtool/XPWork/QQProxy/BaseGuildMsgProxy.java
+++ QTool/src/main/java/cc/hicore/qtool/XPWork/QQProxy/BaseGuildMsgProxy.java
@@ -6,6 +6,7 @@ import cc.hicore.HookItemLoader.Annotations.XPExecutor;
 import cc.hicore.HookItemLoader.Annotations.XPItem;
 import cc.hicore.HookItemLoader.bridge.BaseXPExecutor;
 import cc.hicore.HookItemLoader.bridge.MethodContainer;
+import cc.hicore.HookItemLoader.bridge.MethodFinderBuilder;
 import cc.hicore.HookItemLoader.bridge.QQVersion;
 import cc.hicore.ReflectUtils.Classes;
 import cc.hicore.ReflectUtils.MClass;
@@ -14,11 +15,19 @@ import cc.hicore.qtool.JavaPlugin.Controller.PluginMessageProcessor;
 
 @XPItem(name = "Proxy_Guild_Msg",itemType = XPItem.ITEM_Hook,targetVer = QQVersion.QQ_8_8_35)
 public class BaseGuildMsgProxy{
-    @VerController
+    @VerController(max_targetVer = QQVersion.QQ_8_9_0)
     @MethodScanner
     public void getHookMethod(MethodContainer container){
         container.addMethod("hook_1",MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.guild.message.GuildOnlineMessageProcessor"), null,
                 void.class, new Class[]{Classes.MessageRecord()}));
+        container.addMethod("hook_2",MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.guild.message.api.impl.GuildMessageUtilsApiImpl"), "handleSelfSendMsg",
+                void.class, new Class[]{Classes.AppInterface(), Classes.MessageRecord(), Classes.MessageRecord(), int.class}));
+    }
+    @VerController(targetVer = QQVersion.QQ_8_9_0)
+    @MethodScanner
+    public void getHookMethod_890(MethodContainer container){
+        container.addMethod(MethodFinderBuilder.newFinderByString("hook_1","updateDirectMessageNodeIfNeeded(), message source is invalid! channelId: ",m -> MMethod.FindMethod(m.getDeclaringClass(), null,
+                void.class, new Class[]{Classes.MessageRecord()})));
         container.addMethod("hook_2",MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.guild.message.api.impl.GuildMessageUtilsApiImpl"), "handleSelfSendMsg",
                 void.class, new Class[]{Classes.AppInterface(), Classes.MessageRecord(), Classes.MessageRecord(), int.class}));
     }
