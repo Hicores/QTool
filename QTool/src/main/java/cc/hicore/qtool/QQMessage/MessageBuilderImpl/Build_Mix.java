0205d0ab090837b373f811734fb2b02f349e90ba
Hicores <hicores@163.com>
2022-07-14 18:05:42 +0800

    fix bug

--- QTool/src/main/java/cc/hicore/qtool/QQMessage/MessageBuilderImpl/Build_Mix.java
+++ QTool/src/main/java/cc/hicore/qtool/QQMessage/MessageBuilderImpl/Build_Mix.java
@@ -22,16 +22,38 @@ import cc.hicore.qtool.QQMessage.QQSessionUtils;
 public class Build_Mix {
     CoreLoader.XPItemInfo info;
     @MethodScanner
-    @VerController
+    @VerController(targetVer = QQVersion.QQ_8_9_0)
     public void MethodScaner(MethodContainer container){
         container.addMethod(MethodFinderBuilder.newFinderByString("method"," MessageForWantGiftMsg.GIFT_SENDER_UIN  ", m->true));
     }
 
     @ApiExecutor
-    @VerController
-    public Object build(Object session, ArrayList msgElems) throws Exception {
+    @VerController(targetVer = QQVersion.QQ_8_9_0)
+    public Object build_890(Object session, ArrayList msgElems) throws Exception {
         Method m = null;
         Class<?> clz = info.scanResult.get("method").getDeclaringClass();
+        for (Method ma : clz.getDeclaredMethods()){
+            if (ma.getReturnType().equals(MClass.loadClass("com.tencent.mobileqq.data.MessageForMixedMsg"))){
+                m = ma;
+                break;
+            }
+        }
+
+        Object MixMessageRecord;
+        if (QQSessionUtils.getSessionID(session) == 10014) {
+            MixMessageRecord = m.invoke(null, HookEnv.AppInterface, QQSessionUtils.getChannelID(session), QQEnvUtils.getCurrentUin(), 10014);
+        } else {
+            MixMessageRecord = m.invoke(null, HookEnv.AppInterface, QQSessionUtils.getGroupUin(session), QQEnvUtils.getCurrentUin(), 1);
+        }
+        MField.SetField(MixMessageRecord, "msgElemList", msgElems);
+        MixMessageRecord = MMethod.CallMethodNoParam(MixMessageRecord, "rebuildMixedMsg", MClass.loadClass("com.tencent.mobileqq.data.MessageRecord"));
+        return MixMessageRecord;
+    }
+    @ApiExecutor
+    @VerController(max_targetVer = QQVersion.QQ_8_9_0)
+    public Object build(Object session, ArrayList msgElems) throws Exception {
+        Method m = null;
+        Class<?> clz = MClass.loadClass("com.tencent.mobileqq.service.message.MessageRecordFactory");
         for (Method ma : clz.getDeclaredMethods()){
             if (ma.getReturnType().equals(MClass.loadClass("com.tencent.mobileqq.data.MessageForMixedMsg"))){
                 m = ma;
