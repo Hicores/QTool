f288c011d614f069e110b06d76241c80688d91b8
Hicores <me@hicore.cc>
2022-08-02 09:29:15 +0800

    fix Manager_Group_Kick  on QQ890
    Signed-off-by: Hicores <me@hicore.cc>

--- QTool/src/main/java/cc/hicore/qtool/QQManager/ManagerImpl/Group_Kick.java
+++ QTool/src/main/java/cc/hicore/qtool/QQManager/ManagerImpl/Group_Kick.java
@@ -8,6 +8,7 @@ import cc.hicore.HookItemLoader.Annotations.ApiExecutor;
 import cc.hicore.HookItemLoader.Annotations.VerController;
 import cc.hicore.HookItemLoader.Annotations.XPChecker;
 import cc.hicore.HookItemLoader.Annotations.XPItem;
+import cc.hicore.HookItemLoader.bridge.QQVersion;
 import cc.hicore.ReflectUtils.MClass;
 import cc.hicore.ReflectUtils.MMethod;
 import cc.hicore.Utils.Assert;
@@ -16,12 +17,28 @@ import cc.hicore.qtool.QQManager.QQEnvUtils;
 @XPItem(name = "Manager_Group_Kick",itemType = XPItem.ITEM_Api)
 public class Group_Kick {
     @ApiExecutor
-    @VerController
+    @VerController(max_targetVer = QQVersion.QQ_8_9_0)
     public void kick(String GroupUin, String UserUin, boolean isBlack) throws Exception {
         Object ManagerObject = MClass.NewInstance(MClass.loadClass("com.tencent.mobileqq.troop.handler.TroopMemberMngHandler"), QQEnvUtils.getAppRuntime());
         ArrayList<Long> KickList = new ArrayList<>();
         KickList.add(Long.parseLong(UserUin));
         Method CallMethod = MMethod.FindMethod("com.tencent.mobileqq.troop.handler.TroopMemberMngHandler", null, void.class, new Class[]{
+                long.class,
+                List.class,
+                boolean.class,
+                boolean.class
+        });
+        CallMethod.invoke(ManagerObject,
+                Long.parseLong(GroupUin), KickList, isBlack, false
+        );
+    }
+    @ApiExecutor
+    @VerController(targetVer = QQVersion.QQ_8_9_0)
+    public void kick_new(String GroupUin, String UserUin, boolean isBlack) throws Exception {
+        Object ManagerObject = MClass.NewInstance(MClass.loadClass("com.tencent.mobileqq.troop.handler.l"), QQEnvUtils.getAppRuntime());
+        ArrayList<Long> KickList = new ArrayList<>();
+        KickList.add(Long.parseLong(UserUin));
+        Method CallMethod = MMethod.FindMethod("com.tencent.mobileqq.troop.handler.l", null, void.class, new Class[]{
                 long.class,
                 List.class,
                 boolean.class,
--- QTool/src/main/java/cc/hicore/qtool/QQManager/ManagerImpl/Group_Kick_Multi.java
+++ QTool/src/main/java/cc/hicore/qtool/QQManager/ManagerImpl/Group_Kick_Multi.java
@@ -8,6 +8,7 @@ import cc.hicore.HookItemLoader.Annotations.ApiExecutor;
 import cc.hicore.HookItemLoader.Annotations.VerController;
 import cc.hicore.HookItemLoader.Annotations.XPChecker;
 import cc.hicore.HookItemLoader.Annotations.XPItem;
+import cc.hicore.HookItemLoader.bridge.QQVersion;
 import cc.hicore.ReflectUtils.MClass;
 import cc.hicore.ReflectUtils.MMethod;
 import cc.hicore.Utils.Assert;
@@ -15,13 +16,29 @@ import cc.hicore.qtool.QQManager.QQEnvUtils;
 
 @XPItem(name = "Manager_Group_Kick_Multi",itemType = XPItem.ITEM_Api)
 public class Group_Kick_Multi {
-    @VerController
+    @VerController(max_targetVer = QQVersion.QQ_8_9_0)
     @ApiExecutor
     public void kick_all(String GroupUin, String[] UserUin, boolean isBlack)throws Exception{
         Object ManagerObject = MClass.NewInstance(MClass.loadClass("com.tencent.mobileqq.troop.handler.TroopMemberMngHandler"), QQEnvUtils.getAppRuntime());
         ArrayList<Long> KickList = new ArrayList<>();
         for (String Uin : UserUin) KickList.add(Long.parseLong(Uin));
         Method callMethod = MMethod.FindMethod("com.tencent.mobileqq.troop.handler.TroopMemberMngHandler", null, void.class, new Class[]{
+                long.class,
+                List.class,
+                boolean.class,
+                boolean.class
+        });
+        callMethod.invoke(ManagerObject,
+                Long.parseLong(GroupUin), KickList, isBlack, false
+        );
+    }
+    @VerController(targetVer = QQVersion.QQ_8_9_0)
+    @ApiExecutor
+    public void kick_all_890(String GroupUin, String[] UserUin, boolean isBlack)throws Exception{
+        Object ManagerObject = MClass.NewInstance(MClass.loadClass("com.tencent.mobileqq.troop.handler.l"), QQEnvUtils.getAppRuntime());
+        ArrayList<Long> KickList = new ArrayList<>();
+        for (String Uin : UserUin) KickList.add(Long.parseLong(Uin));
+        Method callMethod = MMethod.FindMethod("com.tencent.mobileqq.troop.handler.l", null, void.class, new Class[]{
                 long.class,
                 List.class,
                 boolean.class,
