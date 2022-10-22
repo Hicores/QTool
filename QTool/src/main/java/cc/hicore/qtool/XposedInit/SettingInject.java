089e38e958520aea7777dd691ed969b68c43ea26
Hicores <me@hicore.cc>
2022-10-22 11:05:40 +0800

    修复平板模式入口消失问题
    Signed-off-by: Hicores <me@hicore.cc>

--- QTool/src/main/java/cc/hicore/qtool/XposedInit/SettingInject.java
+++ QTool/src/main/java/cc/hicore/qtool/XposedInit/SettingInject.java
@@ -3,6 +3,7 @@ package cc.hicore.qtool.XposedInit;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
@@ -23,30 +24,41 @@ import de.robv.android.xposed.XposedBridge;
 
 public class SettingInject {
     public static void startInject(){
+
         Method m = MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.activity.QQSettingSettingActivity"), "doOnCreate", boolean.class, new Class[]{Bundle.class});
-        XPBridge.HookAfter(m,param -> {
+        Method m2 = MMethod.FindMethod(MClass.loadClass("com.tencent.mobileqq.fragment.QQSettingSettingFragment"), "doOnCreateView", void.class, new Class[]{LayoutInflater.class,ViewGroup.class,Bundle.class});
+        XPBridge.AfterHook afterHook = param -> {
             Utils.PostToMainDelay(()->{
                 try {
-                    Activity act = (Activity) param.thisObject;
+                    Activity act = null;
+                    if (param.thisObject instanceof Activity){
+                        act = (Activity) param.thisObject;
+                    }else {
+                        act = (Activity) MMethod.CallMethodNoParam(param.thisObject,"getActivity",MClass.loadClass("androidx.fragment.app.FragmentActivity"));
+                    }
+
                     ResUtils.StartInject(act);
                     ViewGroup mRoot = null;
-                    Class<?> clz = MClass.loadClass("com.tencent.mobileqq.widget.FormSimpleItem");
-                    for (Field f : act.getClass().getDeclaredFields()){
-                        try{
-                            if (clz.equals(f.getType())){
-                                f.setAccessible(true);
-                                View item = (View) f.get(act);
-                                mRoot = (ViewGroup) item.getParent();
-                                if (mRoot instanceof LinearLayout){
-                                    break;
+                    {
+                        Class<?> clz = MClass.loadClass("com.tencent.mobileqq.widget.FormSimpleItem");
+                        for (Field f : param.thisObject.getClass().getDeclaredFields()){
+                            try{
+                                if (clz.equals(f.getType())){
+                                    f.setAccessible(true);
+                                    View item = (View) f.get(param.thisObject);
+                                    mRoot = (ViewGroup) item.getParent();
+                                    if (mRoot instanceof LinearLayout){
+                                        break;
+                                    }
                                 }
-                            }
 
-                        }catch (Exception ignored){
-                            XposedBridge.log(ignored);
+                            }catch (Exception ignored){
+                                XposedBridge.log(ignored);
+                            }
                         }
                     }
-                    View newItem = FormItemUtils.createMultiItem(act, "QTool", BuildConfig.VERSION_NAME, v -> MainMenu.onCreate(act));
+                    Activity finalAct = act;
+                    View newItem = FormItemUtils.createMultiItem(act, "QTool", BuildConfig.VERSION_NAME, v -> MainMenu.onCreate(finalAct));
                     newItem.setOnLongClickListener(v -> {
                         DebugDialog.startShow(v.getContext());
                         return true;
@@ -59,9 +71,10 @@ public class SettingInject {
                         GlobalConfig.Put_Boolean("Add_Menu_Button_to_Main",true);
                     }
                 }
-            },200);
-
-
-        });
+            },200);};
+        XPBridge.HookAfter(m, afterHook);
+        if (m2 != null){
+            XPBridge.HookAfter(m2, afterHook);
+        }
     }
 }
